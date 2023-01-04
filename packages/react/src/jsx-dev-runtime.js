87d2a6c2bbecf703eb9b2cb9b8a7cb6ed23563d4
Fredrik Ekholdt <freekh@gmail.com>
2023-01-04 09:42:03 +0100

    Only deval vals in instrinsic html elements

--- packages/react/src/jsx-dev-runtime.js
+++ packages/react/src/jsx-dev-runtime.js
@@ -1,11 +1,22 @@
 import * as ReactJSXRuntimeDev from "react/jsx-dev-runtime";
 
+const isIntrinsicElement = (type) => {
+  // TODO: think this is not correct, but good enough for now?
+  return typeof type === "string";
+};
+
 export function jsxDEV(type, props, key, isStaticChildren, source, self) {
   console.log("jsxDEV", type, props, key, isStaticChildren, source, self);
 
-  for (const [key, value] of Object.entries(props)) {
-    if (value.val && typeof value.val === "string" && value.id) {
-      props[key] = value.val;
+  if (isIntrinsicElement(type)) {
+    for (const [key, value] of Object.entries(props)) {
+      if (value.val && value.id) {
+        if (typeof value.val === "string") {
+          props[key] = value.val;
+        } else {
+          throw Error("TODO: handle non-string values");
+        }
+      }
     }
   }
 
