9ab1aa297b2e3df15e02ba0986a13371b2b8414c
Fredrik Ekholdt <freekh@gmail.com>
2023-01-04 11:24:08 +0100

    Add val data attribute
    Currently unsure if attributes is the way we want to do this, but we want to start

--- packages/react/src/jsx-dev-runtime.js
+++ packages/react/src/jsx-dev-runtime.js
@@ -8,9 +8,12 @@ const isIntrinsicElement = (type) => {
 export function jsxDEV(type, props, key, isStaticChildren, source, self) {
   console.log("jsxDEV", type, props, key, isStaticChildren, source, self);
 
+  const valIds = [];
+
   if (isIntrinsicElement(type)) {
     for (const [key, value] of Object.entries(props)) {
       if (value.val && value.id) {
+        valIds.push(value.id);
         if (typeof value.val === "string") {
           props[key] = value.val;
         } else {
@@ -18,6 +21,10 @@ export function jsxDEV(type, props, key, isStaticChildren, source, self) {
         }
       }
     }
+  }
+
+  if (valIds.length > 0) {
+    props["data-val-ids"] = valIds.join(",");
   }
 
   return ReactJSXRuntimeDev.jsxDEV(
