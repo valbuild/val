2c86f25701d1ef273e2bd43e87ab9c4c3608f6e9
Fredrik Ekholdt <freekh@gmail.com>
2023-01-12 10:47:30 +0100

    Use id as key in example/next

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -24,6 +24,7 @@ const Home: NextPage = () => {
       >
         {blogs.map((blog) => (
           <section
+            key={blog.id}
             style={{
               display: "flex",
               flexDirection: "column",
