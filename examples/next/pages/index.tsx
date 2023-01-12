ff3e6501b84d19e5b5c1f4d37e80d78300ddc4e3
Fredrik Ekholdt <freekh@gmail.com>
2023-01-12 09:36:03 +0100

    Update example to use array

--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -1,13 +1,9 @@
 import { NextPage } from "next";
 import { useVal } from "../val.config";
-import blog1Val from "./blog1.val";
-import blog2Val from "./blog2.val";
-import blog3Val from "./blog3.val";
+import blogsVal from "./blogs.val";
 
 const Home: NextPage = () => {
-  const blog1 = useVal(blog1Val);
-  const blog2 = useVal(blog2Val);
-  const blog3 = useVal(blog3Val);
+  const blogs = useVal(blogsVal);
   return (
     <main
       style={{
@@ -26,42 +22,20 @@ const Home: NextPage = () => {
           margin: "0 auto",
         }}
       >
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "0 20px",
-          }}
-        >
-          <h1>{blog1.title}</h1>
-          <p>{blog1.text}</p>
-        </section>
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "0 20px",
-          }}
-        >
-          <h1>{blog2.title}</h1>
-          <p>{blog2.text}</p>
-        </section>
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "0 20px",
-          }}
-        >
-          <h1>{blog3.title}</h1>
-          <p>{blog3.text}</p>
-        </section>
+        {blogs.map((blog) => (
+          <section
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "0 20px",
+            }}
+          >
+            <h1>{blog.title}</h1>
+            <p>{blog.text}</p>
+          </section>
+        ))}
       </article>
     </main>
   );
