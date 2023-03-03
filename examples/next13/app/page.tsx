61bfd812c18b806453c4e32941fed4f8536df433
Fredrik Ekholdt <freekh@gmail.com>
2023-03-03 14:20:45 +0100

    Add next13 example

---
+++ examples/next13/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import styles from "./page.module.css";
+import blogsVal from "./blogs.val";
+import { useVal } from "@valbuild/react";
+
+export default function Home() {
+  const blogs = useVal(blogsVal);
+
+  return (
+    <main className={styles.main}>
+      <article className={styles.article}>
+        {blogs.map((blog) => (
+          <section key={blog.valId} className={styles.blog}>
+            <h1>{blog.title}</h1>
+            <p>{blog.text}</p>
+          </section>
+        ))}
+      </article>
+    </main>
+  );
+}
