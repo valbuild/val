fe06ceded05e4c58c89814aa2912b0f27bd35f02
Erlend Åmdal <erlend@aamdal.com>
2023-03-07 14:22:12 +0100

    Add schema deserialization

--- packages/lib/src/schema/SerializedSchema.ts
+++ packages/lib/src/schema/SerializedSchema.ts
@@ -1,8 +1,21 @@
-import { SerializedArraySchema } from "./array";
-import { SerializedObjectSchema } from "./object";
-import { SerializedStringSchema } from "./string";
+import { ValidTypes } from "../ValidTypes";
+import { ArraySchema, SerializedArraySchema } from "./array";
+import { ObjectSchema, SerializedObjectSchema } from "./object";
+import type { Schema } from "./Schema";
+import { SerializedStringSchema, StringSchema } from "./string";
 
 export type SerializedSchema =
   | SerializedStringSchema
   | SerializedObjectSchema
   | SerializedArraySchema;
+
+export function deserialize(schema: SerializedSchema): Schema<ValidTypes> {
+  switch (schema.type) {
+    case "string":
+      return StringSchema.deserialize(schema);
+    case "object":
+      return ObjectSchema.deserialize(schema);
+    case "array":
+      return ArraySchema.deserialize(schema);
+  }
+}
