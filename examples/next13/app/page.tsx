1725e36cc5fc1f9a9fe5c15adcc2d938c267575d
Erlend Åmdal <erlend@aamdal.com>
2023-04-04 09:36:09 +0200

    Update next13 example to use advanced selector functionality

--- examples/next13/app/page.tsx
+++ examples/next13/app/page.tsx
@@ -4,29 +4,28 @@ import blogsVal from "./blogs.val";
 import { useVal } from "@valbuild/react";
 
 export default function Home() {
-  const a = blogsVal.select((blogs) =>
-    blogs.andThen((blogs) =>
-      blogs
-        .filter((blog) => blog.text.eq(null))
-        .map(({ title: title, text }) => ({ title, text }))
+  const blogs = useVal(
+    blogsVal.select((blogs) =>
+      blogs.andThen((blogs) =>
+        blogs
+          .sortBy((blog) => blog.rank)
+          .map((blog) => ({
+            title: blog.title,
+            text: blog.text,
+          }))
+      )
     )
   );
-  const b = a.getVal(a.getModule().content.source, "en_US");
-  if (b.val) {
-    const d = b[0].title;
-  }
-  const c = blogsVal.select((it) => it.andThen((it) => [it[0]]));
-  const blog = useVal(c);
-  const blogs = blog;
   return (
     <main className={styles.main}>
       <article className={styles.article}>
-        {blogs.map((blog) => (
-          <section key={blog.valSrc} className={styles.blog}>
-            <h1>{blog.title}</h1>
-            <p>{blog.text}</p>
-          </section>
-        ))}
+        {blogs.val &&
+          blogs.map((blog) => (
+            <section key={blog.valSrc} className={styles.blog}>
+              <h1>{blog.title}</h1>
+              <p>{blog.text}</p>
+            </section>
+          ))}
       </article>
     </main>
   );
