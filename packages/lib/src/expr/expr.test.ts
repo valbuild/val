93a5017d390e73ccfeda59b2066387e5b329f29a
Erlend Åmdal <erlend@aamdal.com>
2023-04-11 15:05:27 +0200

    Improve tests and coverage

--- packages/lib/src/expr/expr.test.ts
+++ packages/lib/src/expr/expr.test.ts
@@ -15,6 +15,7 @@ import {
   objectLiteral,
   arrayLiteral,
   map,
+  andThen,
 } from "./expr";
 
 const serializationTestCases: {
@@ -152,7 +153,7 @@ test.each(serializationTestCases)("toString $str", ({ str, expr }) => {
 });
 
 describe("evaluateRef", () => {
-  test("simple path yields singular path", () => {
+  test("simple path yields assignable ref", () => {
     const ctx = [
       {
         foo: {
@@ -168,7 +169,7 @@ describe("evaluateRef", () => {
     ]);
   });
 
-  test("slice yields multiple paths", () => {
+  test("slice yields associated refs", () => {
     const ctx = [["foo", "bar", "baz"]] as const;
     const rootExpr = fromCtx<typeof ctx, 0>(0);
     const expr = slice(rootExpr, 1);
@@ -178,7 +179,7 @@ describe("evaluateRef", () => {
     ]);
   });
 
-  test("reverse yields multiple paths", () => {
+  test("reverse yields associated refs", () => {
     const ctx = [[3, 2, 1]] as const;
     const rootExpr = fromCtx<typeof ctx, 0>(0);
     const expr = reverse(rootExpr);
@@ -188,14 +189,14 @@ describe("evaluateRef", () => {
     ]);
   });
 
-  test("reverse item yields singular path", () => {
+  test("item from reverse yields assignable ref", () => {
     const ctx = [[3, 2, 1]] as const;
     const rootExpr = fromCtx<typeof ctx, 0>(0);
     const expr = item(reverse(rootExpr), 0);
     expect(expr.evaluateRef(ctx, [""])).toEqual<ValueAndRef<number>>([1, "/2"]);
   });
 
-  test("sortBy yields multiple paths", () => {
+  test("sortBy yields associated refs", () => {
     const ctx = [[3, 2, 1]] as const;
     const rootExpr = fromCtx<typeof ctx, 0>(0);
     const expr = sortBy(rootExpr, fromCtx(0));
@@ -205,10 +206,160 @@ describe("evaluateRef", () => {
     ]);
   });
 
-  test("sortBy item yields singular path", () => {
+  test("item from sortBy yields assignable ref", () => {
     const ctx = [[3, 2, 1]] as const;
     const rootExpr = fromCtx<typeof ctx, 0>(0);
     const expr = item(sortBy(rootExpr, fromCtx(0)), 0);
     expect(expr.evaluateRef(ctx, [""])).toEqual<ValueAndRef<number>>([1, "/2"]);
+  });
+});
+
+describe("evaluation", () => {
+  test("fromCtx", () => {
+    const input = "foo";
+    const expr = fromCtx<[typeof input], 0>(0);
+    expect(expr.evaluate([input])).toEqual(input);
+    expect(expr.evaluateRef([input], [""])).toEqual([input, ""]);
+  });
+
+  test("prop", () => {
+    const input = {
+      foo: "bar",
+    };
+    const expr = prop(fromCtx<[typeof input], 0>(0), "foo");
+    const expected = input.foo;
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, "/foo"]);
+  });
+
+  test("item", () => {
+    const input = ["foo", "bar"];
+    const expr = item(fromCtx<[typeof input], 0>(0), 1);
+    const expected = input[1];
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, "/1"]);
+  });
+
+  test("filter", () => {
+    const input = ["foo", null, false, "bar"];
+    const expr = filter(fromCtx<[typeof input], 0>(0), fromCtx<[string], 0>(0));
+    const expected = input.filter((i) => i);
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, ["/0", "/3"]]);
+  });
+
+  test("find", () => {
+    const input = ["foo", "bar"];
+    const expr = find(
+      fromCtx<[typeof input], 0>(0),
+      eq(fromCtx<[string], 0>(0), "foo")
+    );
+    const expected = input.find((i) => i === "foo") ?? null;
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, "/0"]);
+  });
+
+  test("slice", () => {
+    const input = ["foo", "bar", "baz"];
+    const expr = slice(fromCtx<[typeof input], 0>(0), 1);
+    const expected = input.slice(1);
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, ["/1", "/2"]]);
+  });
+
+  test("sortBy", () => {
+    const input = [3, 2, 1];
+    const expr = sortBy(fromCtx<[typeof input], 0>(0), fromCtx(0));
+    const expected = input.slice().sort((a, b) => a - b);
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([
+      expected,
+      ["/2", "/1", "/0"],
+    ]);
+  });
+
+  test("reverse", () => {
+    const input = ["foo", "bar", "baz"];
+    const expr = reverse(fromCtx<[typeof input], 0>(0));
+    const expected = input.slice().reverse();
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([
+      expected,
+      ["/2", "/1", "/0"],
+    ]);
+  });
+
+  test("map", () => {
+    const input = [
+      {
+        prop: "foo",
+      },
+      {
+        prop: "bar",
+      },
+      {
+        prop: "baz",
+      },
+    ];
+    const expr = map(fromCtx<[typeof input], 0>(0), prop(fromCtx(0), "prop"));
+    const expected = input.map((v) => v.prop);
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([
+      expected,
+      ["/0/prop", "/1/prop", "/2/prop"],
+    ]);
+  });
+
+  test("eq", () => {
+    const input = "foo";
+    const expr = eq(fromCtx<[typeof input], 0>(0), "foo");
+    const expected = input === "foo";
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, null]);
+  });
+
+  test("andThen", () => {
+    const input: { prop: string } | null = {
+      prop: "foo",
+    };
+    const expr = andThen(
+      fromCtx<[typeof input], 0>(0),
+      prop(fromCtx(0), "prop")
+    );
+    const expected = input?.prop ?? null;
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, "/prop"]);
+  });
+
+  test("primitiveLiteral", () => {
+    const expr = primitiveLiteral<void, 1>(1);
+    const expected = 1;
+    expect(expr.evaluate()).toEqual(expected);
+    expect(expr.evaluateRef()).toEqual([expected, null]);
+  });
+
+  test("objectLiteral", () => {
+    const input = "foo";
+    const expr = objectLiteral<[typeof input], { prop: typeof input }>({
+      prop: fromCtx(0),
+    });
+    const expected = {
+      prop: input,
+    };
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([
+      expected,
+      {
+        prop: "",
+      },
+    ]);
+  });
+
+  test("arrayLiteral", () => {
+    const input = "foo";
+    const expr = arrayLiteral<[typeof input], [typeof input]>([fromCtx(0)]);
+    const expected = [input];
+    expect(expr.evaluate([input])).toEqual(expected);
+    expect(expr.evaluateRef([input], [""])).toEqual([expected, [""]]);
   });
 });
