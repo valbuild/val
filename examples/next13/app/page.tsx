a4f5cf4aee54cb6f489a61bae48a57e43457d42c
Fredrik Ekholdt <freekh@gmail.com>
2023-04-21 15:47:11 +0200

    Added image in next13 example

--- examples/next13/app/page.tsx
+++ examples/next13/app/page.tsx
@@ -2,31 +2,31 @@
 import styles from "./page.module.css";
 import blogsVal from "./blogs.val";
 import { useVal } from "@valbuild/react";
-import { Val } from "@valbuild/lib";
+import { Val, ValImage } from "@valbuild/lib";
 
 export default function Home() {
-  const blogs: Val<{ title: string; text: string | null }[] | null> = useVal(
-    blogsVal.select((blogs) =>
-      blogs.andThen((blogs) =>
+  const blogs: Val<{ title: string; text: string | null; image: ValImage }[]> =
+    useVal(
+      blogsVal.select((blogs) =>
         blogs
           .sortBy((blog) => blog.rank)
           .map((blog) => ({
             title: blog.title,
             text: blog.text,
+            image: blog.image,
           }))
       )
-    )
-  );
+    );
   return (
     <main className={styles.main}>
       <article className={styles.article}>
-        {blogs.val &&
-          blogs.map((blog) => (
-            <section key={blog.valSrc} className={styles.blog}>
-              <h1>{blog.title}</h1>
-              <p>{blog.text}</p>
-            </section>
-          ))}
+        {blogs.map((blog) => (
+          <section key={blog.valSrc} className={styles.blog}>
+            <h1>{blog.title}</h1>
+            <p>{blog.text}</p>
+            <img src={blog.image.url} />
+          </section>
+        ))}
       </article>
     </main>
   );
