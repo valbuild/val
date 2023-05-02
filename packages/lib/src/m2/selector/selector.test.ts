828f9c48a795337666810d0f693551e054f0b568
Fredrik Ekholdt <freekh@gmail.com>
2023-05-02 19:17:52 +0200

    Update selector test

--- packages/lib/src/m2/selector/selector.test.ts
+++ packages/lib/src/m2/selector/selector.test.ts
@@ -1,206 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Selector } from ".";
-import { FileSource, I18nSource } from "../Source";
+import { VAL } from ".";
+import { object } from "../schema/object";
+import { string } from "../schema/string";
+import { array } from "../schema/array";
+import { createSelector } from "./create";
+import { SourcePath } from "../val";
 
-// TODO: create actual test cases - currently testing only type checker
+describe("selector", () => {
+  test("array with string", () => {
+    const sourcePath = "/app/blogs" as SourcePath;
+    const source = ["title1"];
+    const schema = array(string());
 
-{
-  const ex = "" as unknown as Selector<string>;
-  const a = ex.andThen((v) => v);
-  ex.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<undefined>;
-  const a = ex.andThen((v) => "");
-  ex.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<undefined | string>;
-  const a = ex.andThen((v) => v.eq(""));
-  ex.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ bar: string } | undefined>;
-  const a = ex.andThen((v) => v.bar);
-}
-
-{
-  const ex = "" as unknown as Selector<{ bar: string }>;
-  const { bar } = ex;
-  bar.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<
-    ({ title: string; bar: string } | undefined)[]
-  >;
-  const out = ex.map((v) => v);
-  out[0].andThen((v) => v.title).eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<
-    { title: string; bar: { foo: string } | undefined }[]
-  >;
-  const out = ex.map((v) => v);
-  const a = out[0].bar;
-}
-
-{
-  const ex = "" as unknown as Selector<FileSource<string>>;
-  ex.url.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => v);
-  out[0].title;
-  out[0].title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string }[]>;
-  const out = ex.filter((v) => v.title.eq(""));
-  out[0].title;
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => v);
-  out[0].title;
-  out[0].title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => ({
-    subTitle: v.title,
-  }));
-  out[0].subTitle;
-  out[0].subTitle.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => [v.title, v.title]);
-  out[0][0].eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => ({
-    title: {
-      foo: "string",
-    },
-    subTitle: { bar: v.title },
-  }));
-  out[0].title.foo.eq("fdso");
-}
-
-{
-  const ex = "" as unknown as Selector<
-    {
-      title: {
-        foo: {
-          inner: { innerInnerTitle: { even: { more: string } } }[];
-        };
-      };
-      bar: string;
-      many: string[];
-      props: string;
-      are: string;
-      here: { even: { more: string } };
-      for: string;
-      testing: string;
-      purposes: string;
-      and: string;
-      to: string;
-      make: string;
-      sure: string;
-      that: {
-        even: {
-          more: {
-            even: { more: { even: { more: { even: { more: string } } } } }[];
-          };
-        };
-      };
-      the: string;
-      type: string;
-      system: string;
-      works: string;
-      as: string;
-      expected: string;
-    }[]
-  >;
-  const out = ex.map((v) => ({
-    title: {
-      foo: "string",
-    },
-    subTitle: { bar: v },
-  }));
-  out[0].subTitle.bar.that.even.more.even[0].more.even.more.even.more.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => ({ title: { foo: undefined } }));
-}
-
-{
-  const ex = "" as unknown as Selector<{ title: string; bar: string }[]>;
-  const out = ex.map((v) => ({ title1: v.title }));
-}
-
-{
-  const ex = "" as unknown as Selector<I18nSource<"en_US", string>>;
-  ex.eq("");
-}
-
-{
-  const { title } = "" as unknown as Selector<{
-    title: I18nSource<"en_US", string>;
-  }>;
-  title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<I18nSource<"en_US", { title: string }>>;
-  ex.title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<I18nSource<"en_US", { title: string }>>;
-  ex.title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<{
-    foo: I18nSource<"en_US", { title: string }>;
-  }>;
-  ex.foo.title.eq("");
-}
-
-{
-  const ex = "" as unknown as Selector<
-    { type: "foo"; foo: string } | { type: "bar"; bar: number }
-  >;
-  const out = ex.match("type", {
-    foo: (v) => ({ foo: v.foo }),
-    bar: (v) => v.bar,
+    expect(createSelector(sourcePath, schema, source)[0][VAL]()).toStrictEqual({
+      val: "title1",
+      valPath: "/app/blogs.0",
+    });
   });
-}
 
-{
-  const ex = "" as unknown as Selector<
-    ({ type: "foo"; foo: string } | { type: "bar"; bar: number })[]
-  >;
-  const out = ex.map((v) =>
-    v.match("type", {
-      foo: (v) => ({ foo: v.foo }),
-      bar: (v) => v.bar,
-    })
-  );
-}
+  test("array with string", () => {
+    const sourcePath = "/app/blogs" as SourcePath;
+    const source = { title: "title1" };
+    const schema = object({ title: string() });
+
+    expect(
+      createSelector(sourcePath, schema, source).title[VAL]()
+    ).toStrictEqual({ val: "title1", valPath: "/app/blogs.title" });
+  });
+
+  test("array with object", () => {
+    const sourcePath = "/app/blogs" as SourcePath;
+    const source = [{ title: "title1" }];
+    const schema = array(object({ title: string() }));
+    expect(
+      createSelector(sourcePath, schema, source)
+        .map((i) => i.title)
+        [VAL]()[0]
+    ).toStrictEqual({ val: "title1", valPath: "/app/blogs.0.title" });
+  });
+
+  test("array indexed", () => {
+    const sourcePath = "/app/blogs" as SourcePath;
+    const source = [{ title: "title1" }];
+    const schema = array(object({ title: string() }));
+    expect(
+      createSelector(sourcePath, schema, source)
+        .map((i) => i.title)[0]
+        [VAL]()
+    ).toStrictEqual({
+      val: "title1",
+      valPath: "/app/blogs.0.title",
+    });
+  });
+});
