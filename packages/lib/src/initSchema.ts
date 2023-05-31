04afbb6647548125bd433d2eff8204d27b38ed6a
Fredrik Ekholdt <freekh@gmail.com>
2023-05-31 18:25:00 +0200

    Add  s.literal

--- packages/lib/src/initSchema.ts
+++ packages/lib/src/initSchema.ts
@@ -9,6 +9,7 @@ import { union } from "./schema/union";
 import { i18n, I18n } from "./schema/i18n";
 import { richtext } from "./schema/richtext";
 import { image } from "./schema/image";
+import { literal } from "./schema/literal";
 
 export type InitSchema = {
   readonly string: typeof string;
@@ -20,6 +21,7 @@ export type InitSchema = {
   readonly oneOf: typeof oneOf;
   readonly richtext: typeof richtext;
   readonly image: typeof image;
+  readonly literal: typeof literal;
 };
 export type InitSchemaLocalized<Locales extends readonly string[]> = {
   readonly i18n: I18n<Locales>;
@@ -37,6 +39,7 @@ export function initSchema<Locales extends readonly string[]>(
     oneOf,
     richtext,
     image,
+    literal,
     i18n: i18n(locales),
   };
 }
