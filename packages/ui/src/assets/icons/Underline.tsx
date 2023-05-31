cf6fd2aa8b5aabd0552e8ee5e9ce853eff7aad1e
Fredrik Ekholdt <freekh@gmail.com>
2023-05-31 21:58:20 +0200

    Prettier

--- packages/ui/src/assets/icons/Underline.tsx
+++ packages/ui/src/assets/icons/Underline.tsx
@@ -14,9 +14,7 @@ const Underline: FC<{ className?: string }> = ({ className }) => {
       width={25}
       height={25}
     >
-      <path
-        d="M18 4V11C18 14.3137 15.3137 17 12 17C8.68629 17 6 14.3137 6 11V4M4 21H20"
-      />
+      <path d="M18 4V11C18 14.3137 15.3137 17 12 17C8.68629 17 6 14.3137 6 11V4M4 21H20" />
     </svg>
   );
 };
--- packages/ui/src/stories/Checkbox.stories.tsx
+++ packages/ui/src/stories/Checkbox.stories.tsx
@@ -1,17 +1,14 @@
 import { Meta, Story } from "@storybook/react";
 import Checkbox, { CheckboxProps } from "../components/Checkbox";
 
-
 export default {
-    title: "Checkbox",
-    component: Checkbox,
+  title: "Checkbox",
+  component: Checkbox,
 } as Meta;
 
 const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
 
 export const CheckboxStory = Template.bind({});
 CheckboxStory.args = {
-    label: "Check me",
+  label: "Check me",
 };
-
-
