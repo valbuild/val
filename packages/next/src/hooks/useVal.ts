d43166918bb9af2def3bd325f37cab7a26559e5f
Fredrik Ekholdt <freekh@gmail.com>
2023-06-13 15:26:35 +0200

    Use ValEncodedString for all strings

--- packages/next/src/hooks/useVal.ts
+++ packages/next/src/hooks/useVal.ts
@@ -6,17 +6,60 @@ import {
   SelectorSource,
   Val,
   Internal,
+  FileSource,
+  RemoteSource,
+  Source,
+  SourceObject,
 } from "@valbuild/core";
+import { JsonPrimitive } from "@valbuild/core/src/Json";
+import { SourceArray } from "@valbuild/core/src/source";
+import { I18nSource } from "@valbuild/core/src/source/i18n";
 import { useVal as useReactVal } from "@valbuild/react";
 import { vercelStegaCombine } from "@vercel/stega";
+
+declare const brand: unique symbol;
+
+/**
+ * ValEncodedString is a string that is encoded using steganography.
+ *
+ * This means that there is a hidden / non-visible object embedded in the string.
+ * This object includes a path, which is used to automatically tag
+ * where the content comes from for contextual editing.
+ *
+ */
+export type ValEncodedString = string & {
+  [brand]: "ValEncodedString";
+};
+
+type StegaOfSource<T extends Source> = Json extends T
+  ? Json
+  : T extends I18nSource<readonly string[], infer U>
+  ? StegaOfSource<U>
+  : T extends RemoteSource<infer U>
+  ? StegaOfSource<U>
+  : T extends FileSource
+  ? { url: ValEncodedString }
+  : T extends SourceObject
+  ? {
+      [key in keyof T]: StegaOfSource<T[key]>;
+    }
+  : T extends SourceArray
+  ? StegaOfSource<T[number]>[]
+  : T extends string
+  ? ValEncodedString
+  : T extends JsonPrimitive
+  ? T
+  : never;
 
 export function useVal<T extends SelectorSource>(
   selector: T,
   locale?: string
-): SelectorOf<T> extends GenericSelector<infer S> ? JsonOfSource<S> : never {
+): SelectorOf<T> extends GenericSelector<infer S> ? StegaOfSource<S> : never {
   return stegaEncodeVal(
     useReactVal(selector, locale)
-  ) as SelectorOf<T> extends GenericSelector<infer S> ? JsonOfSource<S> : never;
+  ) as SelectorOf<T> extends GenericSelector<infer S>
+    ? StegaOfSource<S>
+    : never;
 }
 
 function stegaEncodeVal<T extends Json>(val: Val<T>): T {
