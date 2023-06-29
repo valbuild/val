dd9711f95d7b25f1a6004155e541bfb7ae2f9298
Fredrik Ekholdt <freekh@gmail.com>
2023-06-29 12:26:12 +0200

    Move dashboard components into dashboard folder

--- packages/ui/src/components/dashboard/DashboardButton.tsx
+++ packages/ui/src/components/dashboard/DashboardButton.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import ExpandLogo from "./ExpandLogo";
+import ExpandLogo from "../ExpandLogo";
 
 export const DashboardButton: FC<{
   onClick: () => void;
@@ -12,7 +12,12 @@ export const DashboardButton: FC<{
       className="py-2 px-3 font-serif text-[12px] tracking-[0.04em] font-[500] border rounded-md text-white dark:border-white border-warm-black bg-warm-black group dark:hover:border-highlight hover:text-highlight "
     >
       <span className="flex flex-row items-center justify-center gap-2">
-        {<ExpandLogo expanded={expanded} className='fill-white group-hover:fill-highlight' />}
+        {
+          <ExpandLogo
+            expanded={expanded}
+            className="fill-white group-hover:fill-highlight"
+          />
+        }
         {children}
       </span>
     </button>
