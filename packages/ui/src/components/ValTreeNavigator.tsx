ae8a947651e7328b8aaa8f45fd133d0f3acc4c77
Fredrik Ekholdt <freekh@gmail.com>
2023-06-29 12:25:23 +0200

    Refactor unused imports, vars, comments - fix typecheck errors ++

--- packages/ui/src/components/ValTreeNavigator.tsx
+++ packages/ui/src/components/ValTreeNavigator.tsx
@@ -1,16 +1,9 @@
 import { Internal, SerializedModule, SourcePath } from "@valbuild/core";
-import { Json, JsonArray, JsonObject } from "@valbuild/core/src/Json";
+import { Json, JsonArray } from "@valbuild/core/src/Json";
 import classNames from "classnames";
-import {
-  Dispatch,
-  DragEvent,
-  FC,
-  SetStateAction,
-  useEffect,
-  useState,
-} from "react";
+import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
 import Chevron from "../assets/icons/Chevron";
-import { JSONValue, PatchJSON } from "@valbuild/core/patch";
+import { PatchJSON } from "@valbuild/core/patch";
 import { ValApi } from "@valbuild/react";
 
 const ValTreeArrayModuleItem: FC<{
@@ -21,41 +14,65 @@ const ValTreeArrayModuleItem: FC<{
   path: string;
   reOrder: (oldIdx: number, newIdx: number) => void;
 }> = ({ submodule, selectedModule, setSelectedModule, idx, path, reOrder }) => {
+  const title = resolveTitle(submodule);
   return (
-    <>
-      <div className="w-fit" draggable id={idx.toString()}>
-        <div className="flex gap-4">
+    <div className="w-fit" draggable id={idx.toString()}>
+      <div className="flex gap-4">
+        <button
+          onClick={() => setSelectedModule(path + idx.toString())}
+          className={classNames(
+            "px-4 py-2 text-start hover:bg-light-gray/20 hover:rounded-lg",
+            {
+              "font-extrabold ":
+                module.path + idx.toString() === selectedModule,
+            }
+          )}
+        >
+          {title}
+        </button>
+        <div className="flex gap-2">
           <button
-            onClick={() => setSelectedModule(path + idx.toString())}
-            className={classNames(
-              "px-4 py-2 text-start hover:bg-light-gray/20 hover:rounded-lg",
-              {
-                "font-extrabold ":
-                  module.path + idx.toString() === selectedModule,
-              }
-            )}
+            onClick={() => reOrder(idx, idx + 1)}
+            className="disabled:text-dark-gray"
           >
-            {submodule.title}
+            <Chevron className="rotate-90" />
           </button>
-          <div className="flex gap-2">
-            <button
-              onClick={() => reOrder(idx, idx + 1)}
-              className="disabled:text-dark-gray"
-            >
-              <Chevron className="rotate-90" />
-            </button>
-            <button
-              onClick={() => reOrder(idx, idx - 1)}
-              className="disabled:text-dark-gray"
-            >
-              <Chevron className="-rotate-90" />
-            </button>
-          </div>
+          <button
+            onClick={() => reOrder(idx, idx - 1)}
+            className="disabled:text-dark-gray"
+          >
+            <Chevron className="-rotate-90" />
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
+const DEFAULT_TITLE = "Untitled";
+function resolveTitle(submodule: Json): string {
+  if (!submodule) {
+    return DEFAULT_TITLE;
+  }
+  if (typeof submodule === "string") {
+    return submodule;
+  }
+  if (typeof submodule === "object") {
+    if ("title" in submodule && typeof submodule.title === "string") {
+      return submodule.title;
+    }
+    if ("name" in submodule && typeof submodule.name === "string") {
+      return submodule.name;
+    }
+    const firstStringField = Object.entries(submodule).find(([, value]) => {
+      return typeof value === "string";
+    })?.[1];
+    if (typeof firstStringField === "string") {
+      return firstStringField;
+    }
+  }
+  return DEFAULT_TITLE;
+}
 
 const ValTreeNavigatorArrayModule: FC<{
   module: SerializedModule;
@@ -68,32 +85,18 @@ const ValTreeNavigatorArrayModule: FC<{
     (module.source as JsonArray).map((submodule) => submodule as Json)
   );
   const [dragId, setDragId] = useState(-1);
-  const [dragging, setDragging] = useState(false);
   const [reOrderMode, setReOrderMode] = useState(false);
-  const [mouse, setMouse] = useState<[number, number]>([0, 0]);
-  const [dropzone, setDropzone] = useState(-1);
-  //Create nice and pretty animation when moving items
 
   useEffect(() => {
     const handler = (e: MouseEvent) => {
       if (dragId !== -1) {
         e.preventDefault();
         setDragId(-1);
-        console.log(dropzone);
       }
     };
     document.addEventListener("mouseup", handler);
     return () => document.removeEventListener("mouseup", handler);
   });
-
-  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-    setDragId(parseInt(e.currentTarget.id));
-    setDragging(true);
-  };
-
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>, idx: number) => {
-    reOrder(dragId, idx);
-  };
 
   const reOrder = async (oldIndex: number, newIndex: number) => {
     const sanitizedNewIndex =
@@ -125,14 +128,6 @@ const ValTreeNavigatorArrayModule: FC<{
       },
     ];
     await valApi.patchModuleContent(moduleId, patch);
-    console.log(
-      selectedModule,
-      path,
-      modulePath,
-      moduleId,
-      newModulePath,
-      newModuleId
-    );
     if (selectedModule === path)
       setSelectedModule(`${newModuleId}.${newModulePath}`);
     setItems((items) => {
@@ -149,11 +144,10 @@ const ValTreeNavigatorArrayModule: FC<{
   };
 
   return (
-    <div className="flex flex-col relative gap-3">
-      {/* {treeIsDirty && <button onClick={saveReorder}>Update tree</button>} */}
-      <div className="flex justify-between items-center ">
+    <div className="relative flex flex-col gap-3">
+      <div className="flex items-center justify-between ">
         <button
-          className="flex justify-between items-center hover:bg-light-gray/20 hover:rounded-lg px-4 py-2 "
+          className="flex items-center justify-between px-4 py-2 hover:bg-light-gray/20 hover:rounded-lg "
           onClick={() => setCollapsed(!collapsed)}
         >
           <div className="flex items-center gap-2">
@@ -166,8 +160,8 @@ const ValTreeNavigatorArrayModule: FC<{
             className="relative w-[20px] h-[20px] flex flex-col justify-between items-center"
             onClick={toggleReorderMode}
           >
-            <Chevron className="-rotate-90 top-0" />
-            <Chevron className=" rotate-90" />
+            <Chevron className="top-0 -rotate-90" />
+            <Chevron className="rotate-90 " />
           </button>
           <button className="text-2xl w-[20px] h-[20px] rounded-full flex justify-center items-center">
             +
@@ -193,13 +187,12 @@ const ValTreeNavigatorArrayModule: FC<{
 
 const ValTreeNavigatorModule: FC<{
   module: SerializedModule;
-  selectedModule: string;
   setSelectedModule: Dispatch<SetStateAction<string>>;
-}> = ({ module, selectedModule, setSelectedModule }) => {
+}> = ({ module, setSelectedModule }) => {
   return (
-    <div className="flex flex-col relative gap-3  hover:bg-light-gray/20 hover:rounded-lg px-4 py-2 ">
+    <div className="relative flex flex-col gap-3 px-4 py-2 hover:bg-light-gray/20 hover:rounded-lg ">
       <button
-        className="flex justify-between items-center"
+        className="flex items-center justify-between"
         onClick={() => setSelectedModule(module.path)}
       >
         <div className="flex items-center gap-2">
@@ -240,7 +233,6 @@ const ValTreeNavigator: FC<ValTreeNavigator> = ({
           ) : (
             <ValTreeNavigatorModule
               module={module}
-              selectedModule={selectedModule}
               setSelectedModule={setSelectedModule}
             />
           )}
