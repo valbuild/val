063beac88b311d9545ebd08d8afb554a8193be79
Fredrik Ekholdt <freekh@gmail.com>
2023-08-15 22:16:11 +0200

    Fix quickJS shims for react/stega and use explicit exports to make it easier to see

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,4 @@
-/* export { useContent } from "./useContent";
-export { useText } from "./useText";
-export { WithVal } from "./WithVal"; */
+// NOTE: the exports of this file needs to be kept in sync with ValQuickJSRuntime
 export { ValProvider } from "./ValProvider";
 export { useVal } from "./hooks/useVal";
+export { ValRichText } from "./ValRichText";
