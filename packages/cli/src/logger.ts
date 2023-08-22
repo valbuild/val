d9f81a7c0237a6d16f302f930e9813c70a03b463
Theodor René Carlsen <theodor.rene@gmail.com>
2023-08-22 09:33:17 +0200

    inital work

--- packages/cli/src/logger.ts
+++ packages/cli/src/logger.ts
@@ -1,9 +1,34 @@
 import chalk from "chalk";
 
-export function error(...message: string[]) {
-  console.error(chalk.red(...message));
+export function error(message: string) {
+  console.error(chalk.red("❌Error: ") + message);
 }
 
-export function info(...message: string[]) {
-  console.log(...message);
+export function info(
+  message: string,
+  opts: { isCodeSnippet?: true; isGood?: true } = {}
+) {
+  if (opts.isCodeSnippet) {
+    console.log(chalk.cyanBright("$ > ") + chalk.cyan(message));
+    return;
+  }
+  if (opts.isGood) {
+    console.log(chalk.green("✅: ") + message);
+    return;
+  }
+  console.log(chalk.blue("️ℹ️ : ") + message);
+}
+
+export function debugPrint(str: string) {
+  /*eslint-disable no-constant-condition */
+  if (process.env["DEBUG"] || true) {
+    // TODO: remove true
+    console.log(`DEBUG: ${str}`);
+  }
+}
+
+export function printDebuggingHelp() {
+  info(
+    `If you're having trouble, please follow the debugging steps\n🌐: https://valbuild.com/docs/troubleshooting`
+  );
 }
