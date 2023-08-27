7016ec1452e14a4a1136e38e99163da8eda078fb
Fredrik Ekholdt <freekh@gmail.com>
2023-08-27 10:25:42 +0200

    Export patch validation

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -12,3 +12,4 @@ export { patchSourceFile } from "./patchValFile";
 export { formatSyntaxErrorTree } from "./patch/ts/syntax";
 export { LocalValServer } from "./LocalValServer";
 export { createFixPatch } from "./createFixPatch";
+export { PatchJSON } from "./patch/validation";
