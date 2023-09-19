446722fd72f0c6c4f364ef4834ae209092ef9b7b
Fredrik Ekholdt <freekh@gmail.com>
2023-09-19 14:07:00 +0200

    Fix patch and form for s.string()

--- packages/ui/src/components/forms/TextArea.tsx
+++ packages/ui/src/components/forms/TextArea.tsx
@@ -2,18 +2,20 @@ export type TextData = string;
 export type TextInputProps = {
   name: string;
   text: TextData;
+  disabled?: boolean;
   onChange: (value: string) => void;
 };
 
-export function TextForm({ name, text, onChange }: TextInputProps) {
+export function TextArea({ name, disabled, text, onChange }: TextInputProps) {
   return (
     <div
-      className="w-full h-full py-2 grow-wrap"
+      className="w-full h-full py-2 overflow-y-scroll grow-wrap"
       data-replicated-value={text} /* see grow-wrap */
     >
       <textarea
+        disabled={disabled}
         name={name}
-        className="p-2 border outline-none bg-fill text-primary border-border focus-visible:border-highlight"
+        className="p-2 border outline-none resize-none bg-fill text-primary border-border focus-visible:border-highlight"
         defaultValue={text}
         onChange={(e) => onChange(e.target.value)}
       />
