566701e9a31a8e2cca69aaffc58eda5e14cfa0c9
Fredrik Ekholdt <freekh@gmail.com>
2023-10-09 14:23:03 +0200

    Remove experimental prop style from ValUI

--- packages/react/src/ValProvider.tsx
+++ packages/react/src/ValProvider.tsx
@@ -30,23 +30,18 @@ export const ValContext = React.createContext<ValContext>({
 
 export type ValProviderProps = {
   host?: string;
-  styles?: string;
   children?: React.ReactNode;
 };
 const ValUI =
   typeof window !== "undefined" ? lazy(() => import("./ValUI")) : null;
 
-export function ValProvider({
-  host = "/api/val",
-  children,
-  styles,
-}: ValProviderProps) {
+export function ValProvider({ host = "/api/val", children }: ValProviderProps) {
   const valApi = useMemo(() => new ValApi(host), [host]);
   const valStore = useMemo(() => new ValStore(valApi), [valApi]);
   return (
     <ValContext.Provider value={{ valApi, valStore }}>
       {children}
-      {ValUI && <ValUI styles={styles} />}
+      {ValUI && <ValUI />}
     </ValContext.Provider>
   );
 }
