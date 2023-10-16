cfba35ba23919bbc5c75fdfc6574cd183a4721a6
Fredrik Ekholdt <freekh@gmail.com>
2023-10-16 22:59:28 +0200

    Create eslint-plugin with no illegal module ids rule

---
+++ packages/eslint-plugin/test/rules/noIllegalModuleIds.test.js
@@ -0,0 +1,73 @@
+import { RuleTester } from "eslint";
+import { rules as valRules } from "@valbuild/eslint-plugin";
+import path from "path";
+
+const rule = valRules["no-illegal-module-ids"];
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-illegal-module-ids", rule, {
+  valid: [
+    {
+      filename: path.join(process.cwd(), "./foo/test.val.ts"),
+      code: `import { val, s } from '../val.config.ts';
+      export const schema = s.string();
+      export default val.content('/foo/test', schema, 'String')`,
+    },
+  ],
+  invalid: [
+    {
+      filename: path.join(process.cwd(), "./foo/test.val.ts"),
+      code: `import { val, s } from '../val.config.ts';
+      export const schema = s.string();
+      export default val.content('foo', schema, 'String')`,
+      errors: [
+        {
+          message: "val.content first argument must match filename",
+        },
+      ],
+      output: `import { val, s } from '../val.config.ts';
+      export const schema = s.string();
+      export default val.content('/foo/test', schema, 'String')`,
+    },
+    {
+      filename: path.join(process.cwd(), "./foo/test.val.ts"),
+      code: `import { val, s } from "../val.config.ts";
+      export const schema = s.string();
+      export default val.content("foo", schema, 'String')`,
+      errors: [
+        {
+          message: "val.content first argument must match filename",
+        },
+      ],
+      output: `import { val, s } from "../val.config.ts";
+      export const schema = s.string();
+      export default val.content("/foo/test", schema, 'String')`,
+    },
+    {
+      filename: path.join(process.cwd(), "./foo/test.val.ts"),
+      code: `import { val, s } from "../val.config.ts";
+      export const schema = s.string();
+      export default val.content(\`foo\`, schema, 'String')`,
+      errors: [
+        {
+          message:
+            "val.content first argument should not be a template literal",
+        },
+      ],
+      output: `import { val, s } from "../val.config.ts";
+      export const schema = s.string();
+      export default val.content("/foo/test", schema, 'String')`,
+    },
+  ],
+});
