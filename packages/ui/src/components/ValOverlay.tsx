fd136c44163ba335c8ab5cea41e8b7dfb7e5ee0f
Fredrik Ekholdt <freekh@gmail.com>
2023-10-25 13:05:50 +0200

    Extracted ValFormField out of ValOverlay

--- packages/ui/src/components/ValOverlay.tsx
+++ packages/ui/src/components/ValOverlay.tsx
@@ -37,6 +37,7 @@ import { readImage } from "../utils/readImage";
 import { lexicalToRichTextSource } from "../richtext/conversion/lexicalToRichTextSource";
 import { ValFullscreen } from "../components/dashboard/ValFullscreen";
 import { RichTextEditor } from "../components/RichTextEditor/RichTextEditor";
+import { ValFormField } from "./ValFormField";
 
 export type ValOverlayProps = {
   defaultTheme?: "dark" | "light";
@@ -141,34 +142,14 @@ export function ValOverlay({ defaultTheme, api }: ValOverlayProps) {
             </div>
             {loading && <div className="text-primary">Loading...</div>}
             {error && <div className="text-red">{error}</div>}
-            {typeof selectedSource === "string" &&
-              selectedSchema?.type === "string" && (
-                <StringField
-                  defaultValue={selectedSource}
-                  isLoading={loading}
-                  registerPatchCallback={initPatchCallback(windowTarget.path)}
-                />
-              )}
-            {selectedSource &&
-              typeof selectedSource === "object" &&
-              VAL_EXTENSION in selectedSource &&
-              selectedSource[VAL_EXTENSION] === "richtext" && (
-                <RichTextField
-                  registerPatchCallback={initPatchCallback(windowTarget.path)}
-                  defaultValue={
-                    selectedSource as RichTextSource<AnyRichTextOptions>
-                  }
-                />
-              )}
-            {selectedSource &&
-              typeof selectedSource === "object" &&
-              VAL_EXTENSION in selectedSource &&
-              selectedSource[VAL_EXTENSION] === "file" && (
-                <ImageField
-                  registerPatchCallback={initPatchCallback(windowTarget.path)}
-                  defaultValue={selectedSource as ImageSource}
-                />
-              )}
+            {selectedSchema !== undefined && selectedSource !== undefined && (
+              <ValFormField
+                disabled={loading}
+                source={selectedSource}
+                schema={selectedSchema}
+                registerPatchCallback={initPatchCallback(windowTarget.path)}
+              />
+            )}
             <div className="flex items-end justify-end py-2">
               <SubmitButton
                 disabled={false}
@@ -201,180 +182,6 @@ export function ValOverlay({ defaultTheme, api }: ValOverlayProps) {
         )}
       </div>
     </ValOverlayContext.Provider>
-  );
-}
-
-type PatchCallback = (modulePath: string) => Promise<PatchJSON>;
-
-function ImageField({
-  defaultValue,
-  registerPatchCallback,
-}: {
-  registerPatchCallback: (callback: PatchCallback) => void;
-  defaultValue?: ImageSource;
-}) {
-  const [data, setData] = useState<string | null>(null);
-  const [metadata, setMetadata] = useState<{
-    width?: number;
-    height?: number;
-    sha256: string;
-  } | null>(null);
-  const url = defaultValue && Internal.convertFileSource(defaultValue).url;
-  useEffect(() => {
-    registerPatchCallback(async (path) => {
-      const pathParts = path.split("/");
-      if (!data) {
-        return [];
-      }
-      return [
-        {
-          value: {
-            ...defaultValue,
-            metadata,
-          },
-          op: "replace",
-          path,
-        },
-        // update the contents of the file:
-        {
-          value: data,
-          op: "replace",
-          path: `${pathParts.slice(0, -1).join("/")}/$${
-            pathParts[pathParts.length - 1]
-          }`,
-        },
-      ];
-    });
-  }, [data]);
-
-  return (
-    <div>
-      <label htmlFor="img_input" className="">
-        <img src={data || url} />
-        <input
-          id="img_input"
-          type="file"
-          hidden
-          onChange={(ev) => {
-            readImage(ev)
-              .then((res) => {
-                setData(res.src);
-                setMetadata({
-                  sha256: res.sha256,
-                  width: res.width,
-                  height: res.height,
-                });
-              })
-              .catch((err) => {
-                console.error(err.message);
-                setData(null);
-                setMetadata(null);
-              });
-          }}
-        />
-      </label>
-    </div>
-  );
-}
-
-function RichTextField({
-  defaultValue,
-  registerPatchCallback,
-}: {
-  registerPatchCallback: (callback: PatchCallback) => void;
-  defaultValue?: RichTextSource<AnyRichTextOptions>;
-}) {
-  const [editor, setEditor] = useState<LexicalEditor | null>(null);
-  useEffect(() => {
-    if (editor) {
-      registerPatchCallback(async (path) => {
-        const { templateStrings, exprs, files } = editor?.toJSON()?.editorState
-          ? await lexicalToRichTextSource(
-              editor?.toJSON()?.editorState.root as LexicalRootNode
-            )
-          : ({
-              [VAL_EXTENSION]: "richtext",
-              templateStrings: [""],
-              exprs: [],
-              files: {},
-            } as RichTextSource<AnyRichTextOptions> & {
-              files: Record<string, string>;
-            });
-        return [
-          {
-            op: "replace" as const,
-            path,
-            value: {
-              templateStrings,
-              exprs,
-              [VAL_EXTENSION]: "richtext",
-            },
-          },
-          ...Object.entries(files).map(([path, value]) => {
-            return {
-              op: "file" as const,
-              path,
-              value,
-            };
-          }),
-        ];
-      });
-    }
-  }, [editor]);
-  return (
-    <RichTextEditor
-      onEditor={(editor) => {
-        setEditor(editor);
-      }}
-      richtext={
-        defaultValue ||
-        ({
-          children: [],
-          [VAL_EXTENSION]: "root",
-        } as unknown as RichTextSource<AnyRichTextOptions>)
-      }
-    />
-  );
-}
-
-function StringField({
-  isLoading,
-  defaultValue,
-  registerPatchCallback,
-}: {
-  registerPatchCallback: (callback: PatchCallback) => void;
-  isLoading: boolean;
-  defaultValue?: string;
-}) {
-  const [value, setValue] = useState(defaultValue || "");
-
-  // ref is used to get the value of the textarea without closing over the value field
-  // to avoid registering a new callback every time the value changes
-  const ref = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    registerPatchCallback(async (path) => {
-      return [
-        {
-          op: "replace",
-          path,
-          value: ref.current?.value || "",
-        },
-      ];
-    });
-  }, []);
-
-  return (
-    <div className="flex flex-col justify-between h-full px-4">
-      <div className="w-full h-full py-2 overflow-y-scroll">
-        <input
-          ref={ref}
-          disabled={isLoading}
-          className="w-full p-2 border outline-none resize-none bg-fill text-primary border-border focus-visible:border-highlight"
-          defaultValue={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </div>
-    </div>
   );
 }
 
