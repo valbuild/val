3d59036960ebee1aef18eb6d549f37c56fc92435
Fredrik Ekholdt <freekh@gmail.com>
2023-11-10 14:08:10 +0100

    Add cjs to fix-server-hack

--- packages/ui/fix-server-hack.js
+++ packages/ui/fix-server-hack.js
@@ -5,35 +5,41 @@
 const fs = require("fs");
 const path = require("path");
 
-const serverFile = "server/dist/valbuild-ui-server.esm.js";
+const serverFiles = [
+  "server/dist/valbuild-ui-server.esm.js",
+  "server/dist/valbuild-ui-server.cjs.js",
+];
 const inputFile = "server/.tmp/index.js";
-const filePath = path.join(__dirname, serverFile);
-const replaceString = "/**REPLACE:SCRIPT*/";
-const fileToEncodePath = path.join(__dirname, inputFile);
 
-// Read the contents of the file to be encoded
-const fileToEncodeContent = fs.readFileSync(fileToEncodePath, "utf-8");
+for (const serverFile of serverFiles) {
+  const filePath = path.join(__dirname, serverFile);
+  const replaceString = "/**REPLACE:SCRIPT*/";
+  const fileToEncodePath = path.join(__dirname, inputFile);
 
-// Encode the file contents to base64
-const encodedContent = Buffer.from(fileToEncodeContent).toString("base64");
+  // Read the contents of the file to be encoded
+  const fileToEncodeContent = fs.readFileSync(fileToEncodePath, "utf-8");
 
-// Read the main file and replace the placeholder string
-fs.readFile(filePath, "utf-8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
+  // Encode the file contents to base64
+  const encodedContent = Buffer.from(fileToEncodeContent).toString("base64");
 
-  const result = data.replace(replaceString, encodedContent);
-
-  // Write the modified content back to the file
-  fs.writeFile(filePath, result, "utf-8", (err) => {
+  // Read the main file and replace the placeholder string
+  fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
-      console.error("Error writing file:", err);
+      console.error("Error reading file:", err);
       return;
     }
-    console.log(
-      `Replaced script in ${serverFile} with contents of ${inputFile}!`
-    );
+
+    const result = data.replace(replaceString, encodedContent);
+
+    // Write the modified content back to the file
+    fs.writeFile(filePath, result, "utf-8", (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log(
+        `Replaced script in ${serverFile} with contents of ${inputFile}!`
+      );
+    });
   });
-});
+}
