e7efad37c014096e19784b949e76d943efdaeafc
Fredrik Ekholdt <freekh@gmail.com>
2023-11-17 10:15:57 +0100

    WIP: use draft mode in initializers

--- examples/next13/val.rsc.ts
+++ examples/next13/val.rsc.ts
@@ -1,6 +1,14 @@
 import { initValRsc } from "@valbuild/next/rsc";
 import { config } from "./val.config";
+import { draftMode, headers } from "next/headers";
 
-const { fetchVal } = initValRsc(config);
+const { fetchVal } = initValRsc(config, {
+  isEnabled: () => {
+    return draftMode().isEnabled;
+  },
+  getHeaders: () => {
+    return headers();
+  },
+});
 
 export { fetchVal };
