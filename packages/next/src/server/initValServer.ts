b47100af78efa49409791b48859e65186c7c8df5
Fredrik Ekholdt <freekh@gmail.com>
2023-11-17 09:45:37 +0100

    Comments

--- packages/next/src/server/initValServer.ts
+++ packages/next/src/server/initValServer.ts
@@ -2,7 +2,10 @@ import { ValConfig } from "@valbuild/core";
 import { createRequestListener } from "@valbuild/server";
 
 const initCreateRequestHandler = (opts: ValConfig) => () =>
-  createRequestListener("/api/val", opts);
+  createRequestListener(
+    "/api/val", // TODO: get from config
+    opts
+  );
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function initValServer(config: ValConfig): {
