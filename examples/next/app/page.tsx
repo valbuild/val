b70b9eae20e65a65b7b269de7df428f51b42c225
Fredrik Ekholdt <freekh@gmail.com>
2023-11-21 17:32:11 +0100

    Update README and examples

--- examples/next/app/page.tsx
+++ examples/next/app/page.tsx
@@ -17,6 +17,33 @@ export default function Home() {
         />
         {<h1 className="h1">{page.hero.title}</h1>}
       </section>
+      <section>
+        {page.text && ( // text is optional
+          <ValRichText
+            theme={{
+              p: "rt-paragraph", // optional
+              // required based on the content:
+              // NOTE: tailwind classes is supported:
+              bold: "font-bold",
+              italic: "font-italic",
+              lineThrough: "line-through",
+              // Classes for tags:
+              li: "rt-list-item",
+              ul: "rt-ul-list",
+              h1: "rt-h1",
+              h2: "rt-h2",
+              h3: "rt-h3",
+              h4: "rt-h4",
+              h5: "rt-h5",
+              h6: "rt-h6",
+              img: "rt-image",
+              ol: "rt-ol-list",
+            }}
+          >
+            {page.text}
+          </ValRichText>
+        )}
+      </section>
     </main>
   );
 }
