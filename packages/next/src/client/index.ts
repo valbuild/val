130ac6c9d1b6755535d0c283c09c62bd6ad8b733
Fredrik Ekholdt <freekh@gmail.com>
2023-11-22 09:40:00 +0100

    Fix merge

--- packages/next/src/client/index.ts
+++ packages/next/src/client/index.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export { initValClient } from "./initValClient";
-=======
 import "client-only";
-export { useVal } from "./useVal";
->>>>>>> a554d723d73492dbd8e43fabea4b2c93b878e75d
+export { initValClient } from "./initValClient";
