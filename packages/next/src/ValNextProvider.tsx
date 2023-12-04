8924fa6b3a860902da7c6d31435d139c10c65ad5
Fredrik Ekholdt <freekh@gmail.com>
2023-12-04 14:34:44 +0100

    Require config for forwards compatibility

--- packages/next/src/ValNextProvider.tsx
+++ packages/next/src/ValNextProvider.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { ValConfig } from "@valbuild/core";
 import { ValProvider as ReactValProvider } from "@valbuild/react/internal";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 
 export const ValNextProvider = (props: {
   children: React.ReactNode | React.ReactNode[];
+  config: ValConfig;
 }) => {
   const router = useRouter();
   const [, startTransition] = useTransition();
