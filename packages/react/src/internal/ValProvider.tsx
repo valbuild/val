70e24c72a46a4b415834f8671db588f33e27e991
Fredrik Ekholdt <freekh@gmail.com>
2023-12-10 09:40:13 +0100

    Show menu in development unless explicitly disabled

--- packages/react/src/internal/ValProvider.tsx
+++ packages/react/src/internal/ValProvider.tsx
@@ -45,7 +45,7 @@ export function ValProvider({ children, onSubmit }: ValProviderProps) {
   const api = useMemo(() => new ValApi(host), [host]);
   const store = useMemo(() => new ValStore(api), [api]);
   const [isClient, setIsClient] = useState(false);
-  const [enabled, setEnabled] = useState(false);
+  const [enabled, setEnabled] = useState<boolean>();
 
   useEffect(() => {
     setIsClient(true);
@@ -53,24 +53,46 @@ export function ValProvider({ children, onSubmit }: ValProviderProps) {
       const valEnabled = document.cookie?.includes(
         `${Internal.VAL_ENABLE_COOKIE_NAME}=true`
       );
-      setEnabled(valEnabled);
+      if (valEnabled) {
+        setEnabled(true);
+      }
+      const valDisabled = document.cookie?.includes(
+        `${Internal.VAL_ENABLE_COOKIE_NAME}=false`
+      );
+      if (valDisabled) {
+        setEnabled(false);
+      }
+      // If we are in development and Val is not enabled or disabled, enable Val
+      if (
+        process.env.NODE_ENV === "development" &&
+        !valEnabled &&
+        !valDisabled
+      ) {
+        window.location.href = `${
+          window.origin
+        }${host}/enable?redirect_to=${encodeURIComponent(
+          window.location.href
+        )}`;
+      }
     } catch (e) {
       console.warn("Could not read Val enabled state", e);
     }
   }, []);
-  if (isClient && !enabled && process.env.NODE_ENV === "development") {
+  if (isClient && !enabled) {
     if (!api) {
       console.warn(
         "Val does not seem to be configured properly! Please check that you have wrapper your root layout (or _app) with the ValProvider."
       );
     } else {
-      console.log(
-        `Val is disabled. Enable it by going here ${window.origin}${
-          api.host
-        }/enable?redirect_to=${encodeURIComponent(
-          window.location.href
-        )}. NOTE: this message appears because NODE_ENV is set to development.`
-      );
+      if (process.env.NODE_ENV === "development") {
+        console.log(
+          `Val is disabled. Enable it by going here: ${window.origin}${
+            api.host
+          }/enable?redirect_to=${encodeURIComponent(
+            window.location.href
+          )}. NOTE: this message appears because NODE_ENV is set to development.`
+        );
+      }
     }
   }
   if (!isClient || !enabled || !store || !api || !ValContext) {
