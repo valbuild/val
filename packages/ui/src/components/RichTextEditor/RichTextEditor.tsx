0a6c457de808edf3807a9c0ac1241b163b0f4fd5
Fredrik Ekholdt <freekh@gmail.com>
2023-12-17 14:43:20 +0100

    Improve looknfeel of richtext

--- packages/ui/src/components/RichTextEditor/RichTextEditor.tsx
+++ packages/ui/src/components/RichTextEditor/RichTextEditor.tsx
@@ -80,7 +80,7 @@ export const RichTextEditor: FC<RichTextEditorProps> = ({
       <RichTextPlugin
         contentEditable={
           <div
-            className="font-sans border-b text-primary border-highlight"
+            className="px-3 py-2 text-sm border-b rounded-md rounded-t-none border-x border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             style={{
               minHeight: windowSize?.innerHeight
                 ? windowSize?.innerHeight - TOOLBAR_HEIGHT
