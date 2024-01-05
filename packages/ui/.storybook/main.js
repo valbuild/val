c2a3094bfbdd582b6fae3248b27bde1b0bbb58a7
Fredrik Ekholdt <freekh@gmail.com>
2024-01-05 09:41:48 +0100

    Update storybook and use SWC with storybook (faster)

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -5,18 +5,13 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: true,
-      },
-    },
+    "@storybook/addon-themes",
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {
+      options: { builder: { useSWC: true } },
+    },
   },
   docs: {
     autodocs: "tag",
