8c4c16f4a3ccc1a367d1e7d87e92c38f933a0b7c
Fredrik Ekholdt <freekh@gmail.com>
2024-01-05 09:16:54 +0100

    Cleanup: rm dashboard and forms (unused)

--- packages/ui/src/components/Grid.stories.tsx
+++ packages/ui/src/components/Grid.stories.tsx
@@ -1,8 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Grid } from "./Grid";
-import { Tree } from "./dashboard/Tree";
-import { Dropdown } from "./dashboard/Dropdown";
-
 const meta: Meta<typeof Grid> = { component: Grid };
 
 export default meta;
@@ -10,19 +7,6 @@ type Story = StoryObj<typeof Grid>;
 export const Default: Story = {
   render: () => (
     <Grid>
-      <Dropdown options={["/blogs", "/journals"]} />
-      <Tree>
-        <Tree.Node path="Main nav" type="section" />
-        <Tree.Node path="H1" type="string">
-          <Tree.Node path="Section 3" type="string" />
-          <Tree.Node path="Section 4" type="section">
-            <Tree.Node path="Section 5" type="string" />
-            <Tree.Node path="Section 6" type="section">
-              <Tree.Node path="Section 7" type="string" />
-            </Tree.Node>
-          </Tree.Node>
-        </Tree.Node>
-      </Tree>
       <div className="flex items-center justify-between w-full h-full px-3 font-serif text-xs text-white">
         <p>Content</p>
         <button className="flex justify-between flex-shrink-0 gap-1">
--- packages/ui/src/exports.ts
+++ packages/ui/src/exports.ts
@@ -3,5 +3,4 @@ export { ValOverlay } from "./components/ValOverlay";
 export { ValFullscreen } from "./components/ValFullscreen";
 
 // Types:
-export { type Inputs } from "./components/forms/Form"; // TODO: delete components/forms/Form
 export { type IValStore } from "./lib/IValStore"; // TODO: move to shared
