aa9a1e2cc5f0bff812ad20b7f6e1b1731015fe2c
Fredrik Ekholdt <freekh@gmail.com>
2024-01-11 19:36:57 +0100

    Test without Style: seems we get about 1 mb less to download

--- packages/react/src/internal/ValUI.tsx
+++ packages/react/src/internal/ValUI.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Style } from "@valbuild/ui";
 import { ShadowRoot } from "./ShadowRoot";
 import { useValApi, useValStore } from "./ValProvider";
 
@@ -30,7 +29,7 @@ export default function ValUI(props: {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&family=Space+Mono:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
-        <Style route={props.host} />
+        <link rel="stylesheet" href={`${"/api/val"}/static/src/index.css`} />{" "}
         <div id="val-overlay"></div>
       </ShadowRoot>
     </>
