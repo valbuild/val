9ea5bcaadf01c90ca80c0492228d44e44ee329ba
Fredrik Ekholdt <freekh@gmail.com>
2024-01-18 11:54:34 +0100

    Improve no patch warning

--- packages/init/src/init.ts
+++ packages/init/src/init.ts
@@ -510,7 +510,7 @@ async function plan(
   // Patches:
 
   const NO_PATCH_WARNING =
-    "Remember to manually patch your pages/_app.tsx file to use Val Provider.\n";
+    "Remember to add the ValProvider in your root layout.tsx / pages/_app.tsx file.\n";
   if (analysis.appRouterLayoutPath) {
     if (!analysis.appRouterLayoutFile) {
       logger.error("Failed to read app router layout file");
