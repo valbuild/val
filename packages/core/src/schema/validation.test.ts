9a38c9084d7fcac7f22fd579b5e8b1dbf6611fec
Fredrik Ekholdt <freekh@gmail.com>
2024-01-19 12:04:43 +0100

    Rm console.log

--- packages/core/src/schema/validation.test.ts
+++ packages/core/src/schema/validation.test.ts
@@ -464,7 +464,6 @@ describe("validation", () => {
     'validate ($description): "$expected"',
     ({ input, schema, expected, fixes }) => {
       const result = schema.validate(testPath, input);
-      console.log(JSON.stringify({ result }, null, 2));
       if (result) {
         expect(Object.keys(result)).toStrictEqual(expected);
         if (fixes) {
