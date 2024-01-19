e0609fd8d0cbdcfb8fd9c258b590c96d5ed70b55
Fredrik Ekholdt <freekh@gmail.com>
2024-01-19 12:22:10 +0100

    Image in richtext now produces c.rt.image

--- packages/core/src/source/file.ts
+++ packages/core/src/source/file.ts
@@ -17,6 +17,7 @@ export type FileSource<
 > = {
   readonly [FILE_REF_PROP]: string;
   readonly [VAL_EXTENSION]: "file";
+  readonly [FILE_REF_SUBTYPE_TAG]?: string;
   readonly metadata?: Metadata;
 };
 
