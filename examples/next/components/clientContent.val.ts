08062666a45e7f17229fc65a5bc289e4f2a048e1
Fredrik Ekholdt <freekh@gmail.com>
2024-01-19 13:52:48 +0100

    Re-export t in config files by default
    If we want to replace it with export type t = initTypes<typeof config>

--- examples/next/components/clientContent.val.ts
+++ examples/next/components/clientContent.val.ts
@@ -1,5 +1,4 @@
-import type { t } from "@valbuild/next";
-import { s, c } from "../val.config";
+import { s, c, type t } from "../val.config";
 
 export const schema = s.object({
   text: s.string(),
--- examples/next/val.config.ts
+++ examples/next/val.config.ts
@@ -4,4 +4,5 @@ const { s, c, val, config } = initVal({
   valCloud: "valbuild/val-next-example",
 });
 
+export type { t } from "@valbuild/next";
 export { s, c, val, config };
