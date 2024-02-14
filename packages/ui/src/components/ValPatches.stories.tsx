462ac216eb238a1c6a7f8d22b2f5498ddee80756
Fredrik Ekholdt <freekh@gmail.com>
2024-02-14 19:28:07 +0100

    Add first draft of new patch review panel

--- packages/ui/src/components/ValPatches.stories.tsx
+++ packages/ui/src/components/ValPatches.stories.tsx
@@ -1,4 +1,9 @@
-import { ReviewPanel as ReviewPanelComponent } from "./ValPatches";
+import { ModuleId } from "@valbuild/core";
+import {
+  History,
+  ReviewErrors,
+  ReviewPanel as ReviewPanelComponent,
+} from "./ValPatches";
 import { ValUIContext } from "./ValUIContext";
 import { Meta, StoryObj } from "@storybook/react";
 
@@ -30,14 +35,144 @@ type Story = StoryObj<typeof ReviewPanelComponent>;
 
 export const ReviewPanel: Story = {
   args: {
-    history: {
-      "module-id": [
-        {
-          path: "path",
-          patchType: "replace",
+    history: [
+      {
+        author: {
+          name: "Fredrik Ekholdt",
+          avatarUrl: "https://randomuser.me/api/portraits/men/3.jpg",
         },
-      ],
-    } as History,
-    errors: {} as ReviewErrors,
+        lastChangedAt: "2024.01.02",
+        changeCount: 4,
+        changes: [
+          {
+            moduleId: "/app/content",
+            items: [
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 3,
+                changedAt: "2024.01.01",
+              },
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 1,
+                changedAt: "2024.01.02",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        lastChangedAt: "3 days ago",
+        changeCount: 4,
+        changes: [
+          {
+            moduleId: "/app/content",
+            items: [
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 3,
+                changedAt: "3 days ago",
+              },
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 1,
+                changedAt: "3 days ago",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        author: {
+          name: "Erlend Åmdal",
+        },
+        lastChangedAt: "yesterday",
+        changeCount: 4,
+        changes: [
+          {
+            moduleId: "/app/content",
+            items: [
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 3,
+                changedAt: "2 days ago",
+              },
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 1,
+                changedAt: "yesterday",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        author: {
+          name: "Fredrik Ekholdt",
+          avatarUrl: "https://randomuser.me/api/portraits/men/3.jpg",
+        },
+        lastChangedAt: "today",
+        changeCount: 1,
+        changes: [
+          {
+            moduleId: "/app/content",
+            items: [
+              {
+                path: '1."title"',
+                type: "replace",
+                count: 1,
+                changedAt: "today",
+              },
+            ],
+          },
+        ],
+      },
+    ] as History,
+    errors: {
+      errors: {
+        "/app/content": [
+          {
+            path: '1."title"',
+            lastChangedBy: {
+              name: "Fredrik Ekholdt",
+              avatarUrl: "https://randomuser.me/api/portraits/men/3.jpg",
+            },
+            lastedChangedAt: "yesterday",
+            messages: [
+              {
+                message:
+                  "Text length is 3 characters: must be between 10 and 100 characters",
+                severity: "warning",
+              },
+              {
+                message:
+                  "Text is 'Hello': must correspond to pattern 'CMS|Test'",
+                severity: "warning",
+              },
+            ],
+          },
+        ] as ReviewErrors["errors"][ModuleId],
+        "/app/example": [
+          {
+            path: '1.0."test"."foo"."long-list".3',
+            lastChangedBy: {
+              name: "Fredrik Ekholdt",
+            },
+            messages: [
+              {
+                severity: "error",
+                message: "Text cannot be empty",
+              },
+            ],
+          },
+        ] as ReviewErrors["errors"][ModuleId],
+      },
+    } as ReviewErrors,
   },
 };
--- packages/ui/src/components/ValPatches.tsx
+++ packages/ui/src/components/ValPatches.tsx
@@ -1,17 +1,28 @@
 import {
+  AnyRichTextOptions,
   ApiPostValidationErrorResponse,
   ApiPostValidationResponse,
+  FileMetadata,
+  ImageMetadata,
   ModuleId,
   PatchId,
+  RichText,
   SourcePath,
   ValApi,
 } from "@valbuild/core";
 import { ChevronDown, Diff, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { result } from "@valbuild/core/fp";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Accordion, AccordionContent } from "./ui/accordion";
-import { AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
+import {
+  AccordionHeader,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+import { Path } from "./Path";
+import classNames from "classnames";
+import { AlertCircle, ChevronRight, ChevronUp, XCircle } from "react-feather";
 
 export type ValPatchesProps = {
   api: ValApi;
@@ -89,30 +100,61 @@ export function ValPatches({
 
 type Author = {
   name: string;
+  avatarUrl?: string;
 };
-type HistoryItem = (
-  | {
-      path: SourcePath;
-      type: "replace" | "move" | "add" | "remove";
-    }
-  | {
-      filename: string;
-    }
-) & {
-  author?: string;
-  date?: string;
+type SourceChangeItem = {
+  path: SourcePath;
+  type: "replace" | "move" | "add" | "remove";
+  count: number;
+  // TODO: display a notification symbol on the last change on a source path with a validation error
+  // notification?: "error" | "warning";
+  // TODO: it would be awesome to get a diff, but too much work for now:
+  // diff?:
+  //   | {
+  //       before: string;
+  //       after: string;
+  //     }
+  //   | {
+  //       before: RichText<AnyRichTextOptions>;
+  //       after: RichText<AnyRichTextOptions>;
+  //     };
+} & {
+  changedAt?: string;
 };
-export type History = [Author, [ModuleId, HistoryItem[]][]];
+type FileChange = {
+  ref: string;
+  type: "replace" | "add";
+};
 
-export type ReviewErrors = [
-  ModuleId,
-  {
-    path: SourcePath;
-    message: string;
-    lastChangedBy: string;
-    lastedChangedAt?: string;
-  }[]
-];
+export type History = {
+  author?: Author;
+  lastChangedAt: string;
+  changeCount: number;
+  changes: (
+    | {
+        moduleId: ModuleId;
+        items: SourceChangeItem[];
+      }
+    | FileChange
+  )[];
+}[];
+
+export type ReviewErrors = {
+  globalError?: string;
+  errors: Record<
+    ModuleId,
+    {
+      moduleError?: string;
+      path: SourcePath;
+      lastChangedBy?: Author;
+      lastedChangedAt?: string;
+      messages: {
+        message: string;
+        severity: "error" | "warning";
+      }[];
+    }[]
+  >;
+};
 
 export function ReviewPanel({
   history,
@@ -123,11 +165,233 @@ export function ReviewPanel({
 }) {
   return (
     <div>
-      <h1 className="block font-sans text-xl font-bold">Review changes</h1>
-      <h2>History</h2>
-      <ul>
-        <li></li>
-      </ul>
+      <h1 className="block mb-6 font-sans text-xl font-bold">Review changes</h1>
+      {history.length > 0 && (
+        <ol>
+          {history.map((item, index) => (
+            <li key={index}>
+              <HistoryItem
+                index={index}
+                last={index === history.length - 1}
+                defaultOpen={history.length > 3 ? false : true}
+              >
+                {item}
+              </HistoryItem>
+            </li>
+          ))}
+        </ol>
+      )}
+      {errors.errors &&
+        Object.entries(errors.errors).map(([moduleId, moduleErrors]) => (
+          <ValidationModuleErrors
+            key={moduleId}
+            moduleId={moduleId as ModuleId}
+          >
+            {moduleErrors}
+          </ValidationModuleErrors>
+        ))}
+    </div>
+  );
+}
+
+function ValidationModuleErrors({
+  moduleId,
+  children: moduleErrors,
+}: {
+  moduleId: ModuleId;
+  children: ReviewErrors["errors"][ModuleId];
+}) {
+  return (
+    <div className="mt-6">
+      <div>
+        <Path>{moduleId}</Path>
+      </div>
+      <div>
+        {moduleErrors.map((error, index) => (
+          <ValidationErrorItem key={index}>{error}</ValidationErrorItem>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function AuthorComponent({ author }: { author?: Author }) {
+  return author?.avatarUrl ? (
+    <img
+      className="w-8 h-8 border rounded-full border-border"
+      src={author.avatarUrl}
+    />
+  ) : (
+    <span className="w-8 h-8 text-sm leading-8 text-center border rounded-full border-border">
+      {author?.name && getInitials(author.name)}
+    </span>
+  );
+}
+
+function ValidationErrorItem({
+  children: error,
+}: {
+  children: ReviewErrors["errors"][ModuleId][number];
+}) {
+  return (
+    <Accordion type="single" collapsible className="text-sm">
+      <AccordionItem value="validation-error">
+        <AccordionTrigger className="flex items-center justify-between w-full mt-3">
+          <span>
+            <span className="min-w-[100px] text-left truncate" dir="rtl">
+              <Path>{error.path}</Path>
+            </span>
+            <span className="ml-2 text-muted">({error.messages.length})</span>
+          </span>
+          <div className="flex items-center gap-4">
+            <span className="truncate">
+              last changed
+              {error.lastedChangedAt ? ` ${error.lastedChangedAt}` : ""} by{" "}
+            </span>
+            {error.lastChangedBy && (
+              <AuthorComponent author={error.lastChangedBy} />
+            )}
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          {error.messages.map((message, index) => {
+            return <ErrorMessage key={index}>{message}</ErrorMessage>;
+          })}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+function ErrorMessage({
+  children: message,
+}: {
+  children: ReviewErrors["errors"][ModuleId][number]["messages"][number];
+}) {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen((open) => !open);
+  return (
+    <button
+      onClick={toggle}
+      className={classNames("flex mt-3 gap-4 max-w-full text-left", {
+        truncate: !open,
+      })}
+    >
+      {message.severity === "warning" ? (
+        <AlertCircle className="flex-shrink-0 text-warning" />
+      ) : (
+        <XCircle className="flex-shrink-0 text-error" />
+      )}
+      {message.message}
+    </button>
+  );
+}
+
+function HistoryItem({
+  index,
+  defaultOpen,
+  last,
+  children: item,
+}: {
+  index: number;
+  last: boolean;
+  defaultOpen?: boolean;
+  children: History[number];
+}) {
+  const [open, setOpen] = useState(defaultOpen);
+  const value = `history-item-${index}`;
+  return (
+    <Accordion
+      type="single"
+      collapsible
+      defaultValue={defaultOpen ? value : undefined}
+      onValueChange={(currentValue) => {
+        setOpen(currentValue === value);
+      }}
+    >
+      <AccordionItem value={value}>
+        <AccordionTrigger className="flex items-start justify-between w-full">
+          <AuthorComponent author={item.author} />
+          <span className="flex items-center gap-4 text-sm">
+            {
+              <span>
+                {item.changeCount}
+                {" changes"}
+                {` • ${item.lastChangedAt}`}
+              </span>
+            }
+            <ChevronDown
+              className={classNames("transition", {
+                "-rotate-180": open,
+              })}
+            />
+          </span>
+        </AccordionTrigger>
+        <ol className="pl-4 mt-4 mb-4 ml-4 border-l border-border">
+          {!open && !last && <div className="h-2"></div>}
+          <AccordionContent>
+            {item.changes.map((change, index) => (
+              <div key={index}>
+                <ChangeItem change={change} />
+              </div>
+            ))}
+          </AccordionContent>
+        </ol>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+function getInitials(name: string) {
+  const [first, ...rest] = name.split(" ");
+  if (rest.length === 0) return first.slice(0, 2);
+  return `${first[0]}${rest.slice(-1)[0]?.[0]}`;
+}
+
+function ChangeItem({
+  change,
+}: {
+  change: History[number]["changes"][number];
+}) {
+  if ("ref" in change) {
+    return (
+      <div>
+        {change.type === "add" ? "Added file " : "Updated file "}
+        {change.ref}
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="font-bold">
+        <Path>{change.moduleId}</Path>
+      </div>
+      <ol className="ml-2">
+        {change.items.map((item, index) => (
+          <li key={index}>
+            <div className="grid grid-cols-3 py-2">
+              <span>
+                <Path>{item.path}</Path>
+              </span>
+              <span>
+                {item.type === "replace"
+                  ? `updated${
+                      item.count
+                        ? item.count > 1
+                          ? ` ${item.count} times`
+                          : ""
+                        : ""
+                    }`
+                  : item.type === "add"
+                  ? "added "
+                  : item.type === "move"
+                  ? "moved "
+                  : "removed "}
+              </span>
+              {item.changedAt && <div>{item.changedAt}</div>}
+            </div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
