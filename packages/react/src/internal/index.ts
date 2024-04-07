db6fad54fc244de0ddec89994325b33fc5b15ddc
Fredrik Ekholdt <freekh@gmail.com>
2024-04-07 08:22:13 +0200

    Remove react/internal ValProvider, ValUI, ...
    ... Replaced by Script block.
    Before moving to script block the host / client application would build the Val Overlay. This resulted in a lot of dependencies: a lot of packages (ca 320mb!) most coming from UI + required that the host app have the same versions of UI packages.
    
    Some day Val might have a UI builder package that lets the host app build their application in React, but that is out of scope for now.

--- packages/react/src/internal/index.ts
+++ packages/react/src/internal/index.ts
@@ -1,3 +1,2 @@
 // NOTE: the exports of this file needs to be kept in sync with ValQuickJSRuntime
-export { ValProvider, useValStore, useValApi } from "./ValProvider";
 export { ValRichText } from "./ValRichText";
