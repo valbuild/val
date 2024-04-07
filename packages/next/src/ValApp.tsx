3c73b55e7137a72874c6d66e03060fa6fe1c8b51
Fredrik Ekholdt <freekh@gmail.com>
2024-04-07 09:41:29 +0200

    Remove HMR that didn't work (in any browser, but gave errors in Firefox)

--- packages/next/src/ValApp.tsx
+++ packages/next/src/ValApp.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ValConfig } from "@valbuild/core";
-import { IS_DEV, VAL_CSS_PATH, VAL_APP_PATH, VAL_APP_ID } from "@valbuild/ui";
+import { VAL_CSS_PATH, VAL_APP_PATH, VAL_APP_ID } from "@valbuild/ui";
 import Script from "next/script";
 
 // eslint-disable-next-line no-empty-pattern
@@ -13,15 +13,6 @@ export const ValApp = ({}: { config: ValConfig }) => {
         rel="stylesheet"
         href={`${route || "/api/val"}/static${VAL_CSS_PATH}`}
       />
-      {IS_DEV && (
-        <Script type="module">{`import RefreshRuntime from "${route}/static/@react-refresh"
-if (RefreshRuntime.injectIntoGlobalHook) {
-  RefreshRuntime.injectIntoGlobalHook(window)
-  window.$RefreshReg$ = () => {}
-  window.$RefreshSig$ = () => (type) => type
-  window.__vite_plugin_react_preamble_installed__ = true
-}`}</Script>
-      )}
       <Script type="module" src={`${route}/static${VAL_APP_PATH}`} />
       <div id={VAL_APP_ID}></div>
     </div>
--- packages/ui/vite.config.mts
+++ packages/ui/vite.config.mts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/api/val/static",
-  plugins: [react()],
   build: {
     minify: true,
     lib: {
