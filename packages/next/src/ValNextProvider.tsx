3c1283131e33bfe90b8a82beba84d074c33ab983
Fredrik Ekholdt <freekh@gmail.com>
2024-04-10 23:13:42 +0200

    Hide Val overlay and ignore events unless Val is enabled

--- packages/next/src/ValNextProvider.tsx
+++ packages/next/src/ValNextProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { ModuleId, ValConfig } from "@valbuild/core";
+import { Internal, ModuleId, ValConfig } from "@valbuild/core";
 import { VAL_APP_PATH, VAL_OVERLAY_ID } from "@valbuild/ui";
 import { usePathname, useRouter } from "next/navigation";
 import Script from "next/script";
-import React from "react";
+import React, { useEffect } from "react";
 import { ValContext, ValEvents } from "./ValContext";
 
 export const ValNextProvider = (props: {
@@ -19,45 +19,73 @@ export const ValNextProvider = (props: {
     return props.children;
   }
   const route = "/api/val";
+
+  // TODO: move below into react package
   const valEvents = React.useMemo(() => new ValEvents(), []);
   const [, startTransition] = React.useTransition();
   const router = useRouter();
+  const [enabled, setEnabled] = React.useState(false);
+
+  useEffect(() => {
+    setEnabled(
+      document.cookie.includes(`${Internal.VAL_ENABLE_COOKIE_NAME}=true`)
+    );
+  }, []);
 
   React.useEffect(() => {
-    const valEventListener = (event: Event) => {
-      if (event instanceof CustomEvent) {
-        if (event.detail.type === "module-update") {
-          const { moduleId, source } = event.detail;
-          if (typeof moduleId === "string" && source !== undefined) {
-            valEvents.update(moduleId as ModuleId, source);
+    if (enabled) {
+      const valEventListener = (event: Event) => {
+        if (event instanceof CustomEvent) {
+          if (event.detail.type === "module-update") {
+            const { moduleId, source } = event.detail;
+            if (typeof moduleId === "string" && source !== undefined) {
+              valEvents.update(moduleId as ModuleId, source);
+            } else {
+              console.error("Val: invalid event detail", event.detail);
+            }
+          } else if (event.detail.type === "overlay-submit") {
+            const { refreshRequired } = event.detail;
+            if (refreshRequired && !props.disableRefresh) {
+              startTransition(() => {
+                router.refresh();
+              });
+            }
           } else {
-            console.error("Val: invalid event detail", event.detail);
+            console.error("Val: invalid event", event);
           }
-        } else if (event.detail.type === "overlay-submit") {
-          const { refreshRequired } = event.detail;
-          if (refreshRequired && !props.disableRefresh) {
-            startTransition(() => {
-              router.refresh();
-            });
-          }
-        } else {
-          console.error("Val: invalid event", event);
         }
-      }
-    };
-    window.addEventListener("val-event", valEventListener);
+      };
+      window.addEventListener("val-event", valEventListener);
 
-    return () => {
-      window.removeEventListener("val-event", valEventListener);
-    };
-  }, []);
+      return () => {
+        window.removeEventListener("val-event", valEventListener);
+      };
+    } else {
+      if (process.env["NODE_ENV"] === "development") {
+        console.warn(
+          `Val is currently hidden.
+
+To enable Val go to the following URL:
+${window.location.origin}/api/val/enable?redirect_to=${encodeURIComponent(
+            window.location.href
+          )}
+          
+You are seeing this message because you are in development mode.`
+        );
+      }
+    }
+  }, [enabled]);
 
   // TODO: use portal to mount overlay
   return (
     <ValContext.Provider value={{ valEvents }}>
       {props.children}
-      <Script type="module" src={`${route}/static${VAL_APP_PATH}`} />
-      <div id={VAL_OVERLAY_ID}></div>
+      {enabled && (
+        <>
+          <Script type="module" src={`${route}/static${VAL_APP_PATH}`} />
+          <div id={VAL_OVERLAY_ID}></div>
+        </>
+      )}
     </ValContext.Provider>
   );
 };
