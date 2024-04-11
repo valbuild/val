3db245e90e675e9640b7646eb949160017afeae6
Fredrik Ekholdt <freekh@gmail.com>
2024-04-11 13:19:00 +0200

    Serve index.html and optimize server program

--- packages/ui/src/vite-server.ts
+++ packages/ui/src/vite-server.ts
@@ -9,13 +9,20 @@ const files: Record<string, string> = JSON.parse(
   `BUILD_REPLACE_THIS_WITH_RECORD`
 ) as unknown as Record<string, string>;
 
+const decodedFiles: Record<string, string> = Object.fromEntries(
+  Object.entries(files).map(([key, value]) => [
+    key,
+    Buffer.from(value, "base64").toString("utf-8"),
+  ])
+);
+
 export function createUIRequestHandler(): ValUIRequestHandler {
   if (typeof files !== "object") {
     throw new Error(
       "Val UI files missing (error: is not an object)! This Val version or build is corrupted!"
     );
   }
-  const jsFiles = Object.keys(files).filter(
+  const jsFiles = Object.keys(decodedFiles).filter(
     (path) => path.endsWith(".js") || path.endsWith(".jsx")
   );
   if (jsFiles.length === 0) {
@@ -31,7 +38,9 @@ export function createUIRequestHandler(): ValUIRequestHandler {
   }
   const MAIN_FILE = jsFiles[0];
 
-  const cssFiles = Object.keys(files).filter((path) => path.endsWith(".css"));
+  const cssFiles = Object.keys(decodedFiles).filter((path) =>
+    path.endsWith(".css")
+  );
   if (cssFiles.length === 0) {
     throw new Error(
       "Val UI files missing (error: no .css files found)! This Val version or build is corrupted!"
@@ -46,7 +55,16 @@ export function createUIRequestHandler(): ValUIRequestHandler {
   const MAIN_CSS_FILE = cssFiles[0];
 
   return async (path, url): Promise<ValServerGenericResult> => {
-    if (path === VAL_APP_PATH) {
+    if (path === "/" || path === "") {
+      return {
+        status: 200,
+        headers: {
+          "Content-Type": getServerMimeType(path) || "",
+          "Cache-Control": "max-age=90", // TODO: change this to something more aggressive
+        },
+        body: decodedFiles["/index.html"],
+      };
+    } else if (path === VAL_APP_PATH) {
       return {
         status: 302,
         redirectTo: url.replace(path, MAIN_FILE),
@@ -62,9 +80,9 @@ export function createUIRequestHandler(): ValUIRequestHandler {
           status: 200,
           headers: {
             "Content-Type": getServerMimeType(path) || "",
-            "Cache-Control": "max-age=90", // TODO: change this to something more aggressive
+            "Cache-Control": "public, max-age=31536000, immutable",
           },
-          body: Buffer.from(files[path], "base64").toString("utf-8"),
+          body: decodedFiles[path],
         };
       } else {
         return {
