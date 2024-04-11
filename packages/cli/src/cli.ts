71155cca3cc99676d41aab48b43da5f3eecf1773
Fredrik Ekholdt <freekh@gmail.com>
2024-04-11 20:15:19 +0200

    Add files command to CLI (useful for cleaning up unused files)

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,21 +1,42 @@
 import meow from "meow";
 import { error } from "./logger";
 import { validate } from "./validate";
+import { files as files } from "./files";
 
 async function main(): Promise<void> {
   const { input, flags, showHelp } = meow(
     `
-      Usage
+      Usage:
         $ val [command]
-      Commands
-        validate val-idate val modules
-
-      Options
+      
+      Options:
         --help                   Show this message
+      
+      Commands:
+        validate
+        list-files
+      
+      Command: validate
+      Description: val-idate val modules
+      Options:
         --root [root], -r [root] Set project root directory (default process.cwd())
         --cfg  [cfg],  -c [cfg]  Set path to config relative to root (default ./val.config)
         --fix  [fix]             Attempt to fix validation errors
-        --noEslint [noEslint]    Disable eslint validation
+        --noEslint [noEslint]    Disable eslint validation during validate
+
+
+      Command: files
+      Description: EXPERIMENTAL.
+        Perform file operations in Val.
+        By default it lists files (images, ...) currently in use by Val. 
+
+        If a managed directory is specified, 
+        it will list all files in the managed directory that ARE NOT currently used by Val.
+        This is useful for cleaning up unused files.
+      Options:
+        --managedDir [dir]      If set, list files found in directory that are not managed by Val
+        --root [root], -r [root] Set project root directory (default process.cwd())
+        --cfg  [cfg],  -c [cfg]  Set path to config relative to root (default ./val.config)
     `,
     {
       flags: {
@@ -38,6 +59,9 @@ async function main(): Promise<void> {
         noEslint: {
           type: "boolean",
         },
+        managedDir: {
+          type: "string",
+        },
       },
       hardRejection: false,
     }
@@ -53,8 +77,22 @@ async function main(): Promise<void> {
 
   const [command] = input;
   switch (command) {
+    case "files":
+      if (flags.fix || flags.noEslint) {
+        return error(
+          `Command "files" does not support --fix or --noEslint flags`
+        );
+      }
+      return files({
+        root: flags.root,
+        cfg: flags.cfg,
+        managedDir: flags.managedDir,
+      });
     case "validate":
     case "idate":
+      if (flags.managedDir) {
+        return error(`Command "validate" does not support --managedDir flag`);
+      }
       return validate({
         root: flags.root,
         cfg: flags.cfg,
