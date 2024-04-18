a555a4aeb08f89d94e4cec0ed5741fc60941ae61
Fredrik Ekholdt <freekh@gmail.com>
2024-04-18 15:45:25 +0200

    Report better errors when path is not correct

--- packages/ui/spa/components/Path.tsx
+++ packages/ui/spa/components/Path.tsx
@@ -16,19 +16,25 @@ export function Path({ children }: { children: string }) {
 }
 
 function Seg({ children: children }: { children: string }) {
-  if (children.includes(".")) {
-    const segs = children.split(".");
-    return segs.map((seg, i) => {
-      if (i !== segs.length - 1) {
-        return (
-          <Fragment key={`${children}.${seg}`}>
-            <span>{i === 0 ? seg : JSON.parse(seg)}</span>
-            <span className="text-muted">{"."}</span>
-          </Fragment>
-        );
-      }
-      return <span key={children}>{i === 0 ? seg : JSON.parse(seg)}</span>;
-    });
+  try {
+    if (children.includes(".")) {
+      const segs = children.split(".");
+      return segs.map((seg, i) => {
+        if (i !== segs.length - 1) {
+          return (
+            <Fragment key={`${children}.${seg}`}>
+              <span>{i === 0 ? seg : JSON.parse(seg)}</span>
+              <span className="text-muted">{"."}</span>
+            </Fragment>
+          );
+        }
+
+        return <span key={children}>{i === 0 ? seg : JSON.parse(seg)}</span>;
+      });
+    }
+    return <span>{children}</span>;
+  } catch (e) {
+    console.error("Parser failure", { children }, e);
+    throw new Error("Unexpected path formatting failure");
   }
-  return <span>{children}</span>;
 }
