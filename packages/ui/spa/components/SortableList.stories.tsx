0ea6c89e30458a6a45935a5d8ac777b248000699
Fredrik Ekholdt <freekh@gmail.com>
2024-04-28 22:59:46 +0200

    On sortable list: add preview record, improve tests, add overflow, ...

--- packages/ui/spa/components/SortableList.stories.tsx
+++ packages/ui/spa/components/SortableList.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { SortableList } from "./SortableList";
 import {
   Schema,
+  SelectorSource,
   SerializedArraySchema,
   SourcePath,
   initVal,
@@ -15,43 +16,86 @@ type Story = StoryObj<typeof SortableList>;
 
 const { s, c } = initVal();
 
-const schema = s.array(
-  s.object({
-    name: s.string(),
-    fields: s.array(s.string()),
-    richtext: s.richtext().nullable(),
-  })
-);
 type SourceOfSchema<S> = S extends Schema<infer Src> ? Src : S;
-const source: SourceOfSchema<typeof schema> = [
-  {
-    name: "Item 1",
-    fields: ["Field 1", "Field 2", "Field 3"],
-    richtext: null,
-  },
-  {
-    name: "Item 2",
-    fields: [],
-    richtext: null,
-  },
-  {
-    name: "Item 3",
-    fields: ["Field 1"],
-    richtext: c.richtext`# Hello world
-
-    ${c.rt.image("/public/test.png")}
-    `,
-  },
-];
+function getArgs<S extends Schema<Array<SelectorSource>>>(
+  schema: S,
+  source: SourceOfSchema<S>
+) {
+  return {
+    path: "/fake" as SourcePath,
+    source,
+    schema: schema.serialize() as SerializedArraySchema,
+  };
+}
 export const Default: Story = {
   render: (args) => (
     <div className="max-w-[800px]">
       <SortableList {...args} />
     </div>
   ),
-  args: {
-    source,
-    path: "/content/items" as SourcePath,
-    schema: schema.serialize() as SerializedArraySchema,
-  },
+  args: getArgs(
+    s.array(
+      s.object({
+        name: s.string(),
+        fields: s.array(s.string()),
+        record: s.record(s.string()),
+        richtext: s.richtext().nullable(),
+        unionStrings: s.union(s.literal("test"), s.literal("test2")),
+        unionObjects: s
+          .union(
+            "type",
+            s.object({ type: s.literal("type1"), name: s.string() })
+          )
+          .nullable(),
+      })
+    ),
+    [
+      {
+        name: "Item 1",
+        fields: ["Field 1", "Field 2", "Field 3"],
+        record: {
+          "Field 1": "Value 1",
+          "Field 2": "Value 2",
+          "Field 3": "Value 3",
+        },
+        richtext: null,
+        unionStrings: "test",
+        unionObjects: null,
+      },
+      {
+        name: "Item 2",
+        fields: [],
+        record: {},
+        richtext: null,
+        unionStrings: "test",
+        unionObjects: {
+          type: "type1",
+          name: "Test",
+        },
+      },
+      {
+        fields: ["Field 1"],
+        name: "Item 3",
+        record: {
+          "Field 1": "Value 1",
+        },
+        richtext: c.richtext`# Hello world
+      
+Test 1 2 3
+${c.rt.image("/public/test.png")}
+    `,
+        unionStrings: "test",
+        unionObjects: null,
+      },
+    ]
+  ),
+};
+
+export const Strings: Story = {
+  render: (args) => (
+    <div className="max-w-[800px]">
+      <SortableList {...args} />
+    </div>
+  ),
+  args: getArgs(s.array(s.string()), ["Item 1", "Item 2", "Item 3"]),
 };
