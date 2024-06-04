8de27137521127e428238f1422aa396687e983f9
Fredrik Ekholdt <freekh@gmail.com>
2024-06-04 21:59:35 +0200

    Simpler authors

--- examples/next/content/authors.val.ts
+++ examples/next/content/authors.val.ts
@@ -3,31 +3,12 @@ import { s, c, type t } from "../val.config";
 export const schema = s.record(
   s.object({
     name: s.string(),
-    bio: s.richtext(),
   })
 );
 
 export type Author = t.inferSchema<typeof schema>;
 export default c.define("/content/authors.val.ts", schema, {
-  freekh: {
-    name: "Fredrik Ekholdt",
-    bio: c.richtext`# Test 1 23
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit
-
-<br />
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit
-
-${c.rt.image("/public/logo_e211b.png")}
-`,
-  },
-  erlamd: {
-    name: "Erlend Åmdal",
-    bio: c.richtext`# Test 1 23`,
-  },
-  teddy: {
-    name: "Theodor René Carlsen",
-    bio: c.richtext`# Test 1 23`,
-  },
+  teddy: { name: "Theodor René Carlsen" },
+  freekh: { name: "Fredrik Ekholdt" },
+  erlamd: { name: "Erlend Åmdal" },
 });
