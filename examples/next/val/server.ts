7b6d26c2655f4a1594056e2a7e6a179a32bdb80a
Fredrik Ekholdt <freekh@gmail.com>
2024-08-13 16:30:41 +0200

    Add formatter to example

--- examples/next/val/server.ts
+++ examples/next/val/server.ts
@@ -3,13 +3,17 @@ import { initValServer } from "@valbuild/next/server";
 import { config } from "../val.config";
 import { draftMode } from "next/headers";
 import valModules from "../val.modules";
+import prettier from "prettier";
 
 const { valNextAppRouter } = initValServer(
   valModules,
   { ...config },
   {
     draftMode,
-  }
+    formatter: (code, filePath) => {
+      return prettier.format(code, { filepath: filePath });
+    },
+  },
 );
 
 export { valNextAppRouter };
