7eeb661adf110dfd2a269b2a8be8c76231f17fb4
Kim Midtlid <kmidtlid3@gmail.com>
2024-09-05 15:50:56 +0200

    Style Record preview cards

--- packages/ui/spa/ng/fields/ImageField.tsx
+++ packages/ui/spa/ng/fields/ImageField.tsx
@@ -21,7 +21,7 @@ export function ImageField({
   }
   return (
     <img
-      src="https://plus.unsplash.com/premium_photo-1712000451123-16ab37e9ff5c?q=80&w=3089&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      src="https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       draggable={false}
       className="object-contain w-full max-h-[500px]"
       style={{
@@ -32,5 +32,14 @@ export function ImageField({
 }
 
 export function ImagePreview({ source }: { source: any }) {
-  return <img src={source} />;
+  return (
+    <img
+      src="https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      draggable={false}
+      className="object-contain w-full max-h-[500px]"
+      style={{
+        cursor: "crosshair",
+      }}
+    />
+  );
 }
