11c21e0b61acee85094e7e187441fb1dc6737f0f
Fredrik Ekholdt <freekh@gmail.com>
2024-09-07 20:30:25 +0200

    Add  a bit more fake data to mock patches/change, errors, deployments...

--- packages/ui/spa/ng/UIProvider.tsx
+++ packages/ui/spa/ng/UIProvider.tsx
@@ -8,6 +8,7 @@ import {
 } from "@valbuild/core";
 import fakeModules from "./fakeContent/val.modules";
 import { Remote } from "../utils/Remote";
+import { Patch } from "@valbuild/core/patch";
 
 const UIContext = React.createContext<{
   getSchemasByModuleFilePath: () => Promise<
@@ -16,6 +17,8 @@ const UIContext = React.createContext<{
   getSourceContent: (moduleFilePath: ModuleFilePath) => Promise<Json>;
   currentSourcePath: SourcePath | ModuleFilePath | null;
   navigate: (path: SourcePath | ModuleFilePath) => void;
+  isPublishing: boolean;
+  setIsPublishing: (isPublishing: boolean) => void;
 }>({
   getSchemasByModuleFilePath: (): never => {
     throw new Error("UIContext not provided");
@@ -25,6 +28,10 @@ const UIContext = React.createContext<{
   },
   currentSourcePath: null,
   navigate: () => {
+    throw new Error("UIContext not provided");
+  },
+  isPublishing: false,
+  setIsPublishing: () => {
     throw new Error("UIContext not provided");
   },
 });
@@ -43,9 +50,13 @@ export function UIProvider({ children }: { children: React.ReactNode }) {
   const [currentSourcePath, setSourcePath] = useState<
     SourcePath | ModuleFilePath | null
   >("/content/basic.val.ts" as SourcePath); // TODO: just testing out /content/basic.val.ts for now
+  // just fake state:
+  const [isPublishing, setIsPublishing] = useState(false);
   return (
     <UIContext.Provider
       value={{
+        isPublishing,
+        setIsPublishing,
         currentSourcePath,
         navigate: setSourcePath,
         getSourceContent: async (
@@ -142,4 +153,192 @@ export function useModuleSource(
   }, [getSourceContent]);
 
   return sourceContent;
+}
+
+// #region Patches
+
+const fakePatches: Record<string, PatchWithMetadata[]> = {
+  "/content/employees/employeeList.val.ts": [
+    {
+      patch_id: "1",
+      author: {
+        name: "k Ekholdt",
+        avatar: "https://avatars.githubusercontent.com/u/91758?s=400&v=4",
+      },
+      created_at: "2021-08-12T12:00:00Z",
+      patch: [
+        {
+          op: "replace",
+          path: ["fe", "name"],
+          value: 'Freddy "The Fish" Fish', // thx copilot
+        },
+      ],
+    },
+    {
+      patch_id: "2",
+      author: {
+        name: "k Ekholdt",
+        avatar: "https://avatars.githubusercontent.com/u/91758?s=400&v=4",
+      },
+      created_at: "2021-08-12T12:00:00Z",
+      patch: [
+        {
+          op: "replace",
+          path: ["fe", "name"],
+          value: "Fredr",
+        },
+      ],
+    },
+    {
+      patch_id: "3",
+      author: {
+        name: "k Ekholdt",
+        avatar: "https://avatars.githubusercontent.com/u/91758?s=400&v=4",
+      },
+      created_at: "2021-08-12T12:00:00Z",
+      patch: [
+        {
+          op: "replace",
+          path: ["fe", "name"],
+          value: "k Ekholdt",
+        },
+      ],
+    },
+  ],
+};
+
+/** Used by fake data so we do not have to type assert all.the.f***king.time. */
+type PatchId = string;
+
+// Fake patch metadata - this type should be replaced with the real one...
+// The UI probably needs render grouped patches, should that be done client side or server side?
+// Pros: client side makes the API more stable, cons: slower UI? Does it even matter?
+type PatchWithMetadata = {
+  patch_id: PatchId;
+  patch: Patch;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  created_at: string;
+};
+export function usePatches() {
+  const [patches, setPatches] = useState<
+    Remote<Record<ModuleFilePath, PatchWithMetadata[]>>
+  >({
+    status: "not-asked",
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setPatches({
+        status: "success",
+        data: fakePatches,
+      });
+    }, 1000); // fake delay
+  }, []);
+  return {
+    patches,
+    deletePatches: (patchIds: PatchId[]) => {
+      setTimeout(() => {
+        setPatches({
+          status: "success",
+          data: Object.fromEntries(
+            Object.entries(fakePatches).map(([path, patches]) => [
+              path,
+              patches.filter((patch) => !patchIds.includes(patch.patch_id)),
+            ])
+          ),
+        });
+      }, 400);
+    },
+    // stashPatches: (patchIds: PatchId[]) => {},
+  };
+}
+
+type ValError =
+  | {
+      type: "validationError";
+      message: string;
+    }
+  | {
+      // should the UI be responsible for "handling" errors? That makes sense right now, but not sure if it will in the future
+      type: "typeError" | "schemaError" | "unknownError";
+      message: string;
+    };
+const fakeErrors: Record<SourcePath, ValError[]> = {
+  [`/content/employees/employeeList.val.ts` as SourcePath]: [
+    {
+      type: "validationError",
+      message: 'Validation error: "name" must be at least 3 characters long',
+    },
+  ],
+};
+export function useErrors() {
+  const [errors, setErrors] = useState<Remote<Record<SourcePath, ValError[]>>>({
+    status: "not-asked",
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setErrors({
+        status: "success",
+        data: fakeErrors,
+      });
+    }, 1000); // fake delay
+  }, []);
+  return { errors };
+}
+
+type DeploymentMetadata = {
+  patch_ids: PatchId[];
+  created_at: string;
+  created_by: {
+    name: string;
+    avatar: string;
+  };
+  triggered_by: "val" | "git-commit";
+};
+const fakeDeployments: DeploymentMetadata[] = [
+  {
+    patch_ids: ["1", "2", "3"],
+    created_at: new Date().toISOString(),
+    created_by: {
+      name: "Fredrik Ekholdt",
+      avatar: "https://avatars.githubusercontent.com/u/91758?s=400&v=4",
+    },
+    triggered_by: "val",
+  },
+];
+export function useDeployments() {
+  const { isPublishing } = useValState();
+  const [deployments, setDeployments] = useState<Remote<DeploymentMetadata[]>>({
+    status: "not-asked",
+  });
+  useEffect(() => {
+    if (isPublishing) {
+      setDeployments({ status: "loading" });
+      setTimeout(() => {
+        setDeployments({
+          status: "success",
+          data: fakeDeployments,
+        });
+      }, 1000); // fake delay
+    }
+  }, [isPublishing]);
+  return { deployments, estimatedDeploymentDurationSeconds: 60 + 30 };
+}
+
+// TODO: do we want a central place where global state should be managed? That would make sense right?
+export function useValState() {
+  const { isPublishing, setIsPublishing } = useContext(UIContext);
+  return {
+    publish: () => {
+      if (!isPublishing) {
+        setIsPublishing(true);
+        setTimeout(() => {
+          setIsPublishing(false);
+        }, 5000); // fake delay
+      }
+    },
+    isPublishing,
+  };
 }
