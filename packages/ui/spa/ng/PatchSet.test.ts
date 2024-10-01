93d377be7c264a6c377e3448872a2146626cabcb
Fredrik Ekholdt <freekh@gmail.com>
2024-10-02 00:15:52 +0200

    Finalize PatchSet a way to present independent sets of patches...
    ... These patch sets give editors more granularity of which patches to publish (instead of having to publish everything).
    
    For example: if editor works on a project in /projects.val.ts then finds an unrelated typo, they can fix typo and publish that without having to publish the new project

---
+++ packages/ui/spa/ng/PatchSet.test.ts
@@ -0,0 +1,325 @@
+import {
+  initVal,
+  Json,
+  ModuleFilePath,
+  PatchId,
+  Schema,
+  SelectorSource,
+} from "@valbuild/core";
+import { Patch } from "@valbuild/core/patch";
+import { PatchSets } from "./PatchSet";
+
+const { s } = initVal();
+describe("PatchSet", () => {
+  test("basic replace", async () => {
+    const patchSet = testPatchSet(
+      "/content/projects.val.ts" as ModuleFilePath,
+      {
+        "Project A": {
+          title: "Title",
+          description: "Description",
+        },
+      },
+      s.record(s.object({ title: s.string(), description: s.string() })),
+      [
+        {
+          patchId: "123" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "title"],
+              value: "New Title",
+            },
+          ],
+        },
+        {
+          patchId: "234" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "description"],
+              value: "New Description",
+            },
+          ],
+        },
+      ],
+    );
+    const expected = {
+      "/content/projects.val.ts": {
+        "/Project A/title": ["123"],
+        "/Project A/description": ["234"],
+      },
+    };
+    expect(patchSet.serialize()).toEqual(expected);
+    expect(PatchSets.from(patchSet.serialize()).serialize()).toEqual(expected);
+  });
+
+  test("basic record replace", async () => {
+    const patchSet = testPatchSet(
+      "/content/projects.val.ts" as ModuleFilePath,
+      {
+        "Project A": {
+          title: "Title",
+          description: "Description",
+        },
+      },
+      s.record(s.object({ title: s.string(), description: s.string() })),
+      [
+        {
+          patchId: "123" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "title"],
+              value: "New Title",
+            },
+          ],
+        },
+        {
+          patchId: "234" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "description"],
+              value: "New Description",
+            },
+          ],
+        },
+        {
+          patchId: "456" as PatchId,
+          patch: [
+            {
+              op: "add",
+              path: ["Project_B"],
+              value: {
+                title: "Title",
+                description: "Description",
+              },
+            },
+          ],
+        },
+        {
+          patchId: "567" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project_B", "description"],
+              value: "Another Description",
+            },
+          ],
+        },
+      ],
+    );
+    const expected = {
+      "/content/projects.val.ts": {
+        "/Project A/title": ["123"],
+        "/Project A/description": ["234"],
+        "/Project_B": ["456", "567"],
+      },
+    };
+    expect(patchSet.serialize()).toEqual(expected);
+    expect(PatchSets.from(patchSet.serialize()).serialize()).toEqual(expected);
+  });
+
+  test("basic array replace", async () => {
+    const patchSet = testPatchSet(
+      "/content/projects.val.ts" as ModuleFilePath,
+      {
+        sections: [
+          {
+            title: "Title",
+            description: "Description",
+          },
+        ],
+      },
+      s.object({
+        sections: s.array(
+          s.object({ title: s.string(), description: s.string() }),
+        ),
+      }),
+      [
+        {
+          patchId: "123" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["sections", "0", "title"],
+              value: "New Title",
+            },
+          ],
+        },
+        {
+          patchId: "234" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["sections", "0", "description"],
+              value: "New Description",
+            },
+          ],
+        },
+      ],
+    );
+    const expected = {
+      "/content/projects.val.ts": {
+        "/sections/0/title": ["123"],
+        "/sections/0/description": ["234"],
+      },
+    };
+    expect(patchSet.serialize()).toEqual(expected);
+    expect(PatchSets.from(patchSet.serialize()).serialize()).toEqual(expected);
+  });
+
+  test("array add", async () => {
+    const patchSet = testPatchSet(
+      "/content/projects.val.ts" as ModuleFilePath,
+      {
+        sections: [
+          {
+            title: "Title",
+            description: "Description",
+          },
+        ],
+      },
+      s.object({
+        sections: s.array(
+          s.object({ title: s.string(), description: s.string() }),
+        ),
+      }),
+      [
+        {
+          patchId: "123" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["sections", "0", "title"],
+              value: "New Title",
+            },
+          ],
+        },
+        {
+          patchId: "234" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["sections", "0", "description"],
+              value: "New Description",
+            },
+          ],
+        },
+        {
+          patchId: "456" as PatchId,
+          patch: [
+            {
+              op: "add",
+              path: ["sections", "0"],
+              value: {
+                title: "Title",
+                description: "Description",
+              },
+            },
+          ],
+        },
+      ],
+    );
+    const expected = {
+      "/content/projects.val.ts": {
+        "/sections": ["123", "234", "456"],
+      },
+    };
+    expect(patchSet.serialize()).toEqual(expected);
+    expect(PatchSets.from(patchSet.serialize()).serialize()).toEqual(expected);
+  });
+
+  test("no patch sets", async () => {
+    const patchSet = testPatchSet(
+      "/content/projects.val.ts" as ModuleFilePath,
+      {
+        "Project A": {
+          title: "Title",
+          description: "Description",
+        },
+      },
+      s.record(s.object({ title: s.string(), description: s.string() })),
+      [
+        {
+          patchId: "123" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "title"],
+              value: "New Title",
+            },
+          ],
+        },
+        {
+          patchId: "234" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project A", "description"],
+              value: "New Description",
+            },
+          ],
+        },
+        {
+          patchId: "456" as PatchId,
+          patch: [
+            {
+              op: "add",
+              path: ["Project_B"],
+              value: {
+                title: "Title",
+                description: "Description",
+              },
+            },
+          ],
+        },
+        {
+          patchId: "567" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: ["Project_B", "description"],
+              value: "Another Description",
+            },
+          ],
+        },
+        {
+          patchId: "678" as PatchId,
+          patch: [
+            {
+              op: "replace",
+              path: [],
+              value: {},
+            },
+          ],
+        },
+      ],
+    );
+    const expected = {
+      "/content/projects.val.ts": ["123", "234", "456", "567", "678"],
+    };
+    expect(patchSet.serialize()).toEqual(expected);
+    expect(PatchSets.from(patchSet.serialize()).serialize()).toEqual(expected);
+  });
+});
+
+function testPatchSet(
+  moduleFilePath: ModuleFilePath,
+  source: Json,
+  schema: Schema<SelectorSource>,
+  patches: TestData[],
+) {
+  const patchSet = new PatchSets();
+  for (const patch of patches) {
+    for (const op of patch.patch) {
+      patchSet.insert(moduleFilePath, source, schema, op, patch.patchId);
+    }
+  }
+  return patchSet;
+}
+type TestData = {
+  patchId: PatchId;
+  patch: Patch;
+};
---
+++ packages/ui/spa/ng/PatchSet.ts
@@ -0,0 +1,261 @@
+import {
+  ModuleFilePath,
+  PatchId,
+  Json,
+  SelectorSource,
+  Internal,
+  ArraySchema,
+  RecordSchema,
+  Schema,
+} from "@valbuild/core";
+import { Operation } from "@valbuild/core/patch";
+
+type PatchSetId = string; // this uses the JSON Patch format a string that looks like this: /path/to/patch
+type PatchPath = string[];
+/**
+ * A patch set is a set of patches that are non-independent of each other. This is useful for example when we want to apply a set of patches atomically.
+ * A patch set is identified by a module file path and a json patch path. The json patch path is a string that looks like this: /path/to/patch
+ * If the modules only contains patch ids, then each patch is its own patch set.
+ */
+export type SerializedPatchSet = Record<
+  ModuleFilePath,
+  | Record<PatchSetId, PatchId[]> // we have patch sets
+  | PatchId[] // no patch sets (each patch is its own patch set)
+>;
+
+/**
+ * This class is used to optimize (and simplify) the process of selecting independent sets of patches.
+ *
+ * The plan is to use the serialized form in the UI, but use the class as a way to make it easier and faster to insert new operations
+ */
+export class PatchSets {
+  private patchSetsByModuleFiles: Record<ModuleFilePath, PatchSetNode>;
+  constructor() {
+    this.patchSetsByModuleFiles = {};
+  }
+
+  reset() {
+    this.patchSetsByModuleFiles = {};
+  }
+
+  insert(
+    moduleFilePath: ModuleFilePath,
+    source: Json,
+    schema: Schema<SelectorSource>,
+    op: Operation,
+    patchId: PatchId,
+  ) {
+    if (!this.patchSetsByModuleFiles[moduleFilePath]) {
+      this.patchSetsByModuleFiles[moduleFilePath] = new PatchSetNode();
+    }
+    const node = this.patchSetsByModuleFiles[moduleFilePath];
+    if (op.op === "file" || op.op === "test") {
+      return;
+    }
+    if (op.op === "replace") {
+      node.insert(op.path, patchId);
+    } else if (
+      op.op === "add" ||
+      op.op === "remove" ||
+      op.op === "move" ||
+      op.op === "copy"
+    ) {
+      try {
+        const { schema: schemaAtPath } = Internal.resolvePath(
+          Internal.patchPathToModulePath(op.path.slice(0, -1)), // get the schema of the parent
+          source,
+          schema,
+        );
+        if (schemaAtPath instanceof ArraySchema) {
+          // for arrays we would need a lot of logic to create a patch set that is not on the entire parent so for now we do just that
+          node.insert(op.path.slice(0, -1), patchId);
+          if (op.op === "move") {
+            node.insert(op.from.slice(0, -1), patchId);
+          }
+        } else if (schemaAtPath instanceof RecordSchema) {
+          node.insert(op.path, patchId);
+          if (op.op === "move") {
+            node.insert(op.from, patchId);
+          }
+        } else {
+          throw new Error(
+            `Cannot perform op: '${op.op}' on non-array or non-record schema. Type: ${
+              schemaAtPath.serialize().type
+            }`,
+          );
+        }
+      } catch (e) {
+        // resolvePath might throw an error if the path is not resolvable
+        // We think this shouldn't happen since we should have already been able to apply patches prior to doing this so this is just us being defensive
+        // We could simply throw the error, but since patch sets is only a client side way to select independent sets of patches we just log the error and continue
+        // These are at least the reason for this as it stands now
+        if (e instanceof Error) {
+          console.error(
+            "Could not resolve path while creating patch set",
+            e.message,
+          );
+        } else {
+          console.error("Could not resolve path while creating patch set", e);
+        }
+        // "terminate" the entire module file path (i.e. all patches are their own patch set)
+        node.insert([], patchId);
+      }
+    } else {
+      const _unreachable: never = op;
+      throw new Error("Unreachable op: " + _unreachable);
+    }
+  }
+
+  serialize(): SerializedPatchSet {
+    const result: SerializedPatchSet = {};
+    for (const moduleFilePathS in this.patchSetsByModuleFiles) {
+      const moduleFilePath = moduleFilePathS as ModuleFilePath;
+      const moduleRadixNode = this.patchSetsByModuleFiles[moduleFilePath];
+      if (moduleRadixNode.terminal) {
+        result[moduleFilePath] = moduleRadixNode.serialize()[""]; // NOTE: on the node level the root is "" and this means we that there are no patch sets within this module. To make it clearer we explicitly set the entire module to be an array of patch ids in this case
+      } else {
+        result[moduleFilePath] = moduleRadixNode.serialize();
+      }
+    }
+    return result;
+  }
+
+  static from(serialized: SerializedPatchSet) {
+    const patchSets = new PatchSets();
+    for (const moduleFilePathS in serialized) {
+      const moduleFilePath = moduleFilePathS as ModuleFilePath;
+      const moduleRadixNode = PatchSetNode.from(serialized[moduleFilePath]);
+      patchSets.patchSetsByModuleFiles[moduleFilePath] = moduleRadixNode;
+    }
+    return patchSets;
+  }
+}
+
+class PatchSetNode {
+  /** undefined for root node */
+  private key?: string; // undefined for root node
+  private children: Record<string, PatchSetNode>;
+  /**
+   * terminal means that there exists a patch path that ends in this node
+   * For example if we replace on /foo/bar then /foo/bar is a terminal node.
+   * NOTE: if there is first a replace on /foo/bar then on /foo (both are terminal), but the serialized form will merge all sub paths of /foo into /foo
+   **/
+  terminal: boolean;
+  patchIds: PatchId[];
+
+  constructor(key?: string) {
+    this.key = key;
+    this.terminal = false;
+    this.children = {};
+    this.patchIds = [];
+  }
+
+  insert(patchPath: PatchPath, patchId: PatchId) {
+    if (this.key) {
+      throw new Error("Cannot insert into a root (non-keyed) node");
+    }
+    if (patchPath.length === 0) {
+      this.terminal = true;
+      this.patchIds.push(patchId);
+    } else {
+      let child: PatchSetNode = this as PatchSetNode;
+      for (const key of patchPath) {
+        if (isPrototypePollution(key)) {
+          throw new Error(
+            "Prototype pollution attempt in patch path: " +
+              [""].concat(patchPath).join("/"),
+          );
+        }
+        if (!child.children[key]) {
+          child.children[key] = new PatchSetNode(key);
+        }
+        child = child.children[key];
+        if (child.terminal) {
+          // At this point we could clean up sub-tree and move patchIds into this node
+          // However, we take this into account as we serialize currently so that would just be an optimization (or not, depending on how often we serialize vs. insert)
+          break;
+        }
+      }
+      child.patchIds.push(patchId);
+      child.terminal = true;
+    }
+  }
+
+  serialize(): Record<PatchSetId, PatchId[]> {
+    const result: Record<PatchSetId, PatchId[]> = {};
+    function go(
+      node: PatchSetNode,
+      path: string[],
+      terminalPath: string[] | null,
+    ) {
+      let nodeTerminalPath = terminalPath;
+      if (!nodeTerminalPath && node.terminal) {
+        nodeTerminalPath = path;
+      }
+      for (const childKey in node.children) {
+        const child = node.children[childKey];
+        if (!child.key) {
+          throw new Error(
+            "Cannot serialize a non-keyed, non-root node: " + path,
+          );
+        }
+        go(child, path.concat(child.key), nodeTerminalPath);
+      }
+      if (nodeTerminalPath) {
+        const jsonTerminalPath = nodeTerminalPath.join("/");
+        if (!result[jsonTerminalPath]) {
+          // NOTE: we always copy the array to avoid mutating the internal patch set state:
+          result[jsonTerminalPath] = [...node.patchIds];
+        } else {
+          result[jsonTerminalPath].push(...node.patchIds);
+        }
+      }
+    }
+    if (this.key) {
+      throw new Error("Cannot serialize a non-root node");
+    }
+    for (const childKey in this.children) {
+      const child = this.children[childKey];
+      go(child, ["", childKey], this.terminal ? [""] : null);
+    }
+    if (this.terminal) {
+      if (!result[""]) {
+        result[""] = [];
+      }
+      result[""].push(...this.patchIds);
+    }
+    return result;
+  }
+
+  static from(serialized: SerializedPatchSet[ModuleFilePath]) {
+    if (Array.isArray(serialized)) {
+      const node = new PatchSetNode();
+      node.terminal = true;
+      node.patchIds = serialized;
+      return node;
+    }
+    const root = new PatchSetNode();
+    for (const key in serialized) {
+      let child = root;
+      // skip first empty string (paths start with /)
+      for (const keyPart of key.split("/").slice(1)) {
+        if (isPrototypePollution(keyPart)) {
+          throw new Error("Prototype pollution attempt in: " + key);
+        }
+        // skip first empty string (paths start with /)
+        if (!child.children[keyPart]) {
+          child.children[keyPart] = new PatchSetNode(keyPart);
+        }
+        child = child.children[keyPart];
+      }
+      child.terminal = true;
+      child.patchIds = serialized[key];
+    }
+    return root;
+  }
+}
+
+function isPrototypePollution(key: string): boolean {
+  // We want to use record to serialize quickly, but we do not want prototype pollution:
+  return key === "__proto__" || key === "constructor" || key === "prototype";
+}
