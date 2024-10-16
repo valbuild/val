01c72953cfec3c5a1c996b1ffd3d83a3558dfebe
Fredrik Ekholdt <freekh@gmail.com>
2024-10-16 18:03:38 +0200

    WIP: upating UnionFields to next gen state mgmt

--- packages/ui/spa/ng/fields/UnionField.tsx
+++ packages/ui/spa/ng/fields/UnionField.tsx
@@ -1,13 +1,254 @@
-import { LiteralSchema, SourcePath, UnionSchema } from "@valbuild/core";
+import {
+  LiteralSchema,
+  SerializedLiteralSchema,
+  SerializedObjectUnionSchema,
+  SerializedStringUnionSchema,
+  SerializedUnionSchema,
+  SourcePath,
+} from "@valbuild/core";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+import {
+  useAddPatch,
+  useSchemaAtPath,
+  useShallowSourceAtPath,
+} from "../ValProvider";
+import { FieldLoading } from "../components/FieldLoading";
+import { FieldNotFound } from "../components/FieldNotFound";
+import { FieldSchemaError } from "../components/FieldSchemaError";
+import { FieldSchemaMismatchError } from "../components/FieldSchemaMismatchError";
+import { FieldSourceError } from "../components/FieldSourceError";
+import { Field } from "../components/Field";
+import { emptyOf } from "../../components/fields/emptyOf";
+import { AnyField } from "../components/AnyField";
+import { sourcePathOfItem } from "../../utils/sourcePathOfItem";
+import { JSONValue } from "@valbuild/core/patch";
+import { isJsonArray } from "../../utils/isJsonArray";
 
 function isStringUnion(
-  schema: UnionSchema<LiteralSchema<string> | string, any>,
-): schema is UnionSchema<LiteralSchema<string>, any> {
-  return schema.key instanceof LiteralSchema;
+  schema: SerializedUnionSchema,
+): schema is SerializedStringUnionSchema {
+  if (typeof schema.key === "string") {
+    return false;
+  }
+  return true;
 }
 
 export function UnionField({ path }: { path: SourcePath }) {
-  return <div>TODO union field</div>;
+  const type = "union";
+  const schemaAtPath = useSchemaAtPath(path);
+  const sourceAtPath = useShallowSourceAtPath(path, type);
+  if (schemaAtPath.status === "error") {
+    return (
+      <FieldSchemaError path={path} error={schemaAtPath.error} type={type} />
+    );
+  }
+  if (sourceAtPath.status === "error") {
+    return (
+      <FieldSourceError path={path} error={sourceAtPath.error} type={type} />
+    );
+  }
+  if (
+    sourceAtPath.status == "not-found" ||
+    schemaAtPath.status === "not-found"
+  ) {
+    return <FieldNotFound path={path} type={type} />;
+  }
+  if (schemaAtPath.status === "loading") {
+    return <FieldLoading path={path} type={type} />;
+  }
+  if (!("data" in sourceAtPath) || sourceAtPath.data === undefined) {
+    return <FieldLoading path={path} type={type} />;
+  }
+  if (schemaAtPath.data.type !== type) {
+    return (
+      <FieldSchemaMismatchError
+        path={path}
+        expectedType={type}
+        actualType={schemaAtPath.data.type}
+      />
+    );
+  }
+
+  const source = sourceAtPath.data;
+  if (isStringUnion(schemaAtPath.data)) {
+    if (typeof source !== "string" && source !== null) {
+      return (
+        <FieldSourceError
+          path={path}
+          error={"Expected source to be a string, but found: " + typeof source}
+          type={type}
+        />
+      );
+    }
+    return (
+      <SelectField
+        path={path}
+        source={source}
+        options={schemaAtPath.data.items.flatMap((item) => {
+          if (item?.type === "literal") {
+            return [item.value];
+          }
+          console.warn("Unexpected item in string union", item);
+          return [];
+        })}
+      />
+    );
+  } else if (!isStringUnion(schemaAtPath.data)) {
+    if (typeof source !== "object") {
+      return (
+        <FieldSourceError
+          path={path}
+          error={"Expected source to be an object, but found: " + typeof source}
+          type={type}
+        />
+      );
+    }
+    if (Array.isArray(source)) {
+      return (
+        <FieldSourceError
+          path={path}
+          error={"Expected source to be an object, but found an array"}
+          type={type}
+        />
+      );
+    }
+    return <ObjectUnionField path={path} schema={schemaAtPath.data} />;
+  }
+}
+
+function ObjectUnionField({
+  path,
+  schema,
+}: {
+  path: SourcePath;
+  schema: SerializedObjectUnionSchema;
+}) {
+  const keyPath = sourcePathOfItem(path, schema.key);
+  const { addPatch, patchPath } = useAddPatch(path);
+  const currentSourceKeyRes = useShallowSourceAtPath(keyPath, "literal");
+  const schemaAtPath = useSchemaAtPath(path);
+  if (
+    !("data" in currentSourceKeyRes) ||
+    currentSourceKeyRes.data === undefined
+  ) {
+    return <FieldLoading path={path} type="union" />;
+  }
+  const selectedSchema = schema.items.find((item) => {
+    const subSchema = item.items?.[schema.key];
+    if (subSchema.type === "literal") {
+      return subSchema.value === currentSourceKeyRes.data;
+    }
+    console.warn("Expected literal schema in object union", subSchema);
+    return false;
+  });
+  if (selectedSchema?.items === undefined) {
+    return <FieldLoading path={path} type="union" />;
+  }
+
+  const options = schema.items.flatMap((item) => {
+    const subSchema = item.items?.[schema.key];
+    if (subSchema.type === "literal") {
+      return [subSchema.value];
+    }
+    console.warn("Expected literal schema in object union", subSchema);
+    return [];
+  });
+  return (
+    <div className="grid gap-4">
+      <Select
+        value={currentSourceKeyRes.data ?? undefined}
+        onValueChange={(value) => {
+          const newValue: any = emptyOf(schema);
+          newValue[schema.key] = value;
+          addPatch([
+            {
+              op: "replace",
+              path: patchPath,
+              value: newValue,
+            },
+          ]);
+        }}
+      >
+        <SelectTrigger>
+          <SelectValue>{currentSourceKeyRes.data}</SelectValue>
+        </SelectTrigger>
+        <SelectContent className="w-32">
+          {options == undefined ? (
+            <LoadingSelectContent />
+          ) : (
+            options.map((index) => (
+              <SelectItem key={index} value={index}>
+                {index}
+              </SelectItem>
+            ))
+          )}
+        </SelectContent>
+      </Select>
+      {Object.keys(selectedSchema.items)
+        .filter((key) => key !== schema.key)
+        .map((key) => {
+          const itemPath = sourcePathOfItem(path, key);
+          return (
+            <AnyField
+              key={key}
+              path={itemPath}
+              schema={selectedSchema.items[key]}
+            />
+          );
+        })}
+    </div>
+  );
+}
+
+function SelectField({
+  path,
+  source,
+  options,
+}: {
+  path: SourcePath;
+  source: string | null;
+  options?: string[];
+}) {
+  const { addPatch, patchPath } = useAddPatch(path);
+  return (
+    <Select
+      value={source ?? ""}
+      onValueChange={(value) => {
+        addPatch([
+          {
+            op: "replace",
+            path: patchPath,
+            value: value,
+          },
+        ]);
+      }}
+    >
+      <SelectTrigger>
+        <SelectValue>{source}</SelectValue>
+      </SelectTrigger>
+      <SelectContent className="w-32">
+        {options == undefined ? (
+          <LoadingSelectContent />
+        ) : (
+          options.map((index) => (
+            <SelectItem key={index} value={index}>
+              {index}
+            </SelectItem>
+          ))
+        )}
+      </SelectContent>
+    </Select>
+  );
+}
+
+function LoadingSelectContent() {
+  return <div>Loading...</div>;
 }
 
 export function UnionPreview({ path }: { path: SourcePath }) {
