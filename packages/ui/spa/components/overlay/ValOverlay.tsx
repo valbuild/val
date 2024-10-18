a98d70bb2df0577371712ed5dcf9d9a31bb4c7ef
Fredrik Ekholdt <freekh@gmail.com>
2024-10-18 22:13:26 +0200

    Update ValOverlay with draggable Val menu

--- packages/ui/spa/components/overlay/ValOverlay.tsx
+++ packages/ui/spa/components/overlay/ValOverlay.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { Eye, Play, Search, Upload } from "lucide-react";
 import { useState } from "react";
 
 export type ValOverlayProps = {
@@ -18,6 +19,48 @@ type DropZones =
   | "val-menu-right-bottom";
 
 export function ValOverlay() {
+  return <DraggableValMenu />;
+}
+
+function ValMenu({
+  dropZone,
+  ghost,
+}: {
+  dropZone: DropZones;
+  ghost?: boolean;
+}) {
+  const dir =
+    dropZone === "val-menu-right-center" || dropZone === "val-menu-left-center"
+      ? "vertical"
+      : "horizontal";
+  return (
+    <div className="p-4 ">
+      <div
+        className={classNames(
+          "flex relative rounded bg-bg-primary text-text-primary gap-4",
+          {
+            "flex-col py-4 px-2": dir === "vertical",
+            "flex-row px-4 py-2": dir === "horizontal",
+            "opacity-70": ghost,
+          },
+        )}
+      >
+        <button className="">
+          <Eye size={24} />
+        </button>
+        <button>
+          <Search size={24} />
+        </button>
+        <button>
+          <Upload size={24} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function DraggableValMenu() {
+  const [isDragging, setIsDragging] = useState(false);
   const [dropZone, setDropZone] = useState<DropZones>("val-menu-right-center");
   const [dragOverDropZone, setDragOverDropZone] = useState<DropZones | null>(
     null,
@@ -30,77 +73,117 @@ export function ValOverlay() {
     event.preventDefault();
     setDragOverDropZone(id);
   };
+
   return (
     <>
-      <div className="fixed top-0 left-0 grid w-screen h-screen grid-cols-3 grid-rows-3 z-[5]">
-        <DropZone
-          id="val-menu-left-top"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-center-top"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-right-top"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-left-center"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <div></div>
-        <DropZone
-          id="val-menu-right-center"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-left-bottom"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-center-bottom"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
-        <DropZone
-          id="val-menu-right-bottom"
-          dropZone={dropZone}
-          dragOverDropZone={dragOverDropZone}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-        ></DropZone>
+      <div
+        className={classNames("z-overlay", getPositionClassName(dropZone))}
+        draggable
+        onDragStart={() => {
+          setIsDragging(true);
+        }}
+        onDragEnd={() => {
+          setIsDragging(false);
+        }}
+      >
+        <ValMenu dropZone={dropZone} />
       </div>
+      {isDragging && (
+        <>
+          {dragOverDropZone && dragOverDropZone !== dropZone && (
+            <div
+              className={classNames(
+                "z-[3]",
+                getPositionClassName(dragOverDropZone),
+              )}
+              onDrop={onDrop(dragOverDropZone)}
+              onDragOver={onDragOver(dragOverDropZone)}
+            >
+              <ValMenu dropZone={dragOverDropZone} ghost />
+            </div>
+          )}
+          <div className="fixed top-0 left-0 grid w-screen h-screen grid-cols-3 grid-rows-3 z-[2]">
+            <DropZone
+              id="val-menu-left-top"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-center-top"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-right-top"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-left-center"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <div></div>
+            <DropZone
+              id="val-menu-right-center"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-left-bottom"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-center-bottom"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+            <DropZone
+              id="val-menu-right-bottom"
+              dropZone={dropZone}
+              dragOverDropZone={dragOverDropZone}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+            ></DropZone>
+          </div>
+        </>
+      )}
     </>
   );
+}
+
+function getPositionClassName(dropZone: DropZones) {
+  return classNames("fixed transform", {
+    "left-0 top-0": dropZone === "val-menu-left-top",
+    "left-0 top-1/2 -translate-y-1/2": dropZone === "val-menu-left-center",
+    "left-0 bottom-0": dropZone === "val-menu-left-bottom",
+    "left-1/2 -translate-x-1/2 top-0": dropZone === "val-menu-center-top",
+    "left-1/2 -translate-x-1/2 bottom-0": dropZone === "val-menu-center-bottom",
+    "right-0 top-0": dropZone === "val-menu-right-top",
+    "right-0 top-1/2 -translate-y-1/2": dropZone === "val-menu-right-center",
+    "right-0 bottom-0": dropZone === "val-menu-right-bottom",
+  });
 }
 
 const DropZone = ({
   id,
   onDrop,
   onDragOver,
-  dropZone,
-  dragOverDropZone,
 }: {
   id: DropZones;
   onDrop: (id: DropZones) => (event: React.DragEvent) => void;
@@ -108,104 +191,5 @@ const DropZone = ({
   dropZone: DropZones;
   dragOverDropZone: DropZones | null;
 }) => {
-  const dir =
-    id === "val-menu-right-center" || id === "val-menu-left-center"
-      ? "vertical"
-      : "horizontal";
-
-  let className = "p-4";
-  if (id === "val-menu-left-top") {
-    className += classNames(
-      className,
-      "flex justify-start items-start bg-red-100",
-    );
-  } else if (id === "val-menu-left-center") {
-    className += classNames(
-      className,
-      "flex justify-start items-center bg-red-200",
-    );
-  } else if (id === "val-menu-left-bottom") {
-    className += classNames(
-      className,
-      "flex justify-start items-end bg-red-300",
-    );
-  } else if (id === "val-menu-center-top") {
-    className += classNames(
-      className,
-      "flex justify-center items-start bg-red-400",
-    );
-  } else if (id === "val-menu-center-bottom") {
-    className += classNames(
-      className,
-      "flex justify-center items-end bg-red-500",
-    );
-  } else if (id === "val-menu-right-top") {
-    className += classNames(
-      className,
-      "flex justify-end items-start bg-red-600",
-    );
-  } else if (id === "val-menu-right-center") {
-    className += classNames(
-      className,
-      "flex justify-end items-center bg-red-700",
-    );
-  } else if (id === "val-menu-right-bottom") {
-    className += classNames(className, "flex justify-end items-end bg-red-800");
-  } else {
-    const _exhaustiveCheck: never = id;
-    console.error('Unexpected drop zone: "', _exhaustiveCheck, '"');
-  }
-  return (
-    <div onDrop={onDrop(id)} onDragOver={onDragOver(id)} className={className}>
-      {dragOverDropZone === id && dropZone !== id && (
-        <ValMenuContainer dir={dir} id={id} ghost />
-      )}
-      {dropZone === id && <ValMenuContainer dir={dir} id={id} />}
-    </div>
-  );
+  return <div onDrop={onDrop(id)} onDragOver={onDragOver(id)}></div>;
 };
-
-function ValMenuContainer({
-  dir,
-  ghost,
-  id,
-}: {
-  dir: "horizontal" | "vertical";
-  ghost?: boolean;
-  id: DropZones;
-}) {
-  let className = "";
-  if (dir === "horizontal") {
-    className = classNames(className, "w-40 h-10");
-  } else if (dir === "vertical") {
-    className = classNames(className, "w-10 h-40");
-  }
-
-  if (id.startsWith("val-menu-left-")) {
-    className = classNames(className, "left-0");
-  } else if (id.startsWith("val-menu-right-")) {
-    className = classNames(className, "right-0");
-  } else if (id.startsWith("val-menu-center-")) {
-    className = classNames(className, "left-1/2 transform -translate-x-1/2");
-  }
-  if (id.endsWith("-top")) {
-    className = classNames(className, "top-0");
-  } else if (id.endsWith("-bottom")) {
-    className = classNames(className, "bottom-0");
-  } else if (id.endsWith("-center")) {
-    className = classNames(className, "top-1/2 transform -translate-y-1/2");
-  }
-  if (ghost) {
-    className = classNames(className, "bg-slate-200");
-  } else {
-    className = classNames(className, "bg-slate-700");
-  }
-  return (
-    <div className="relative">
-      <div
-        className={classNames(className, "absolute")}
-        draggable={!ghost}
-      ></div>
-    </div>
-  );
-}
