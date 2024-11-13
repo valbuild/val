269d526c327e3f695501eaf03f07eba38e1c81e2
Fredrik Ekholdt <freekh@gmail.com>
2024-11-13 13:17:54 +0100

    Format

--- packages/ui/spa/utils/sourcePathOfItem.ts
+++ packages/ui/spa/utils/sourcePathOfItem.ts
@@ -3,7 +3,7 @@ import { SourcePath } from "@valbuild/core";
 // TODO: move this to @valbuild/core and replace createValPathOfItem with this.
 export function sourcePathOfItem(
   path: string,
-  item: string | number
+  item: string | number,
 ): SourcePath {
   if (path.includes("?p=")) {
     return (path + "." + JSON.stringify(item)) as SourcePath;
