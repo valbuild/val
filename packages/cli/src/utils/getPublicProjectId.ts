144a15e9bdd4e3b7ff926e8374197c4412a938d8
Fredrik Ekholdt <freekh@gmail.com>
2025-01-26 14:49:10 +0100

    Implement get public project id...
    ... endpoint is now ready

--- packages/cli/src/utils/getPublicProjectId.ts
+++ packages/cli/src/utils/getPublicProjectId.ts
@@ -14,34 +14,30 @@ export async function getPublicProjectId(
     }
 > {
   try {
-    // const response = await fetch(`${host}/project/${projectName}`, {
-    //   headers: {
-    //     "X-VAL-AUTH-PAT": `Bearer ${pat}`,
-    //   },
-    // });
-    // if (response.status === 404) {
-    //   return {
-    //     success: false,
-    //     error: `Project ${projectName} not found. Verify that user has access to project and that it exists.`,
-    //   };
-    // }
-    // if (response.status !== 200) {
-    //   return {
-    //     success: false,
-    //     error: `Failed to get project id: ${response.statusText}`,
-    //   };
-    // }
-    // const json = await response.json();
-    // if (typeof json?.publicProjectId !== "string") {
-    //   return {
-    //     success: false,
-    //     error: `Invalid response: missing public project id`,
-    //   };
-    // }
-    const fakeJson = {
-      publicProjectId: "dd7df2d6",
-    };
-    const json = fakeJson;
+    const response = await fetch(`${host}/v1/${projectName}/settings`, {
+      headers: {
+        "X-VAL-PAT": pat,
+      },
+    });
+    if (response.status === 404) {
+      return {
+        success: false,
+        error: `Project ${projectName} not found. Verify that user has access to project and that it exists.`,
+      };
+    }
+    if (response.status !== 200) {
+      return {
+        success: false,
+        error: `Failed to get project id: ${response.statusText}`,
+      };
+    }
+    const json = await response.json();
+    if (typeof json?.publicProjectId !== "string") {
+      return {
+        success: false,
+        error: `Invalid response: missing public project id`,
+      };
+    }
     return {
       success: true,
       data: { publicProjectId: json.publicProjectId },
