694f7bb9ae2ae619052857fac6f998cc2344701e
Fredrik Ekholdt <freekh@gmail.com>
2025-04-02 09:53:41 +0200

    Extract canMerge function of mergePatches

--- packages/ui/spa/utils/mergePatches.ts
+++ packages/ui/spa/utils/mergePatches.ts
@@ -16,19 +16,25 @@ export function mergePatches(
     const sortedPatches = patches.sort((a, b) => a.seqNumber - b.seqNumber);
     for (let i = 0; i < sortedPatches.length; i++) {
       const { patch } = sortedPatches[i];
-      if (patch.length === 1 && patch[0].op === "replace") {
-        const nextPatch = sortedPatches[i + 1]?.patch;
-        if (
-          nextPatch &&
-          nextPatch.length === 1 &&
-          nextPatch[0].op === "replace" &&
-          nextPatch[0].path.join("/") === patch[0].path.join("/")
-        ) {
-          continue;
-        }
+      if (canMerge(patch, sortedPatches[i + 1]?.patch)) {
+        continue;
       }
       mergedPatches.push({ patch, path: moduleFilePath });
     }
   }
   return mergedPatches;
+}
+
+export function canMerge(last: Patch | undefined, next: Patch | undefined) {
+  if (!last || !next) {
+    return false;
+  }
+  // Currently the only case we merge is when there's 1 op in both patches and they both replace the same value
+  // This is a pretty common use case, since every keystroke is a patch
+  if (last.length === 1 && last[0].op === "replace") {
+    if (next.length === 1 && next[0].op === "replace") {
+      return last[0].path.join("/") === next[0].path.join("/");
+    }
+  }
+  return false;
 }
