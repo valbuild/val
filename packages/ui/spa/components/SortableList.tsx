8f451ae04a0de3827dbc6958551bcabb421d5533
Fredrik Ekholdt <freekh@gmail.com>
2025-04-09 15:32:46 +0200

    Style record / array items

--- packages/ui/spa/components/SortableList.tsx
+++ packages/ui/spa/components/SortableList.tsx
@@ -181,7 +181,7 @@ export function SortableItem({
       {schema?.item?.type === "string" && <StringField path={path} />}
       {schema?.item?.type !== "string" && (
         <button
-          className="relative grid p-4 overflow-hidden text-left border rounded border-border bg-card gap-y-2 grid-cols-subgrid cols-span-1 bg-bg-primary"
+          className="relative grid p-4 overflow-hidden text-left border rounded border-border bg-card gap-y-2 grid-cols-subgrid cols-span-1 bg-bg-primary hover:bg-bg-brand-primary hover:text-text-brand-primary"
           style={{
             maxHeight: LIST_ITEM_MAX_HEIGHT,
           }}
