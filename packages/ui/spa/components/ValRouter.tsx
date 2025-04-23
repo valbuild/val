e26b3b2809aeaa84e972c27d250be2be289a40d5
Fredrik Ekholdt <freekh@gmail.com>
2025-04-23 10:12:05 +0200

    Support hash with source path so we can go from Overlay and behave similarly as if it was clicked internally in Studio

--- packages/ui/spa/components/ValRouter.tsx
+++ packages/ui/spa/components/ValRouter.tsx
@@ -32,6 +32,13 @@ const ValRouterContext = React.createContext<ValRouterContextValue>(
 
 const VAL_CONTENT_VIEW_ROUTE = "/val/~"; // TODO: make route configurable
 
+function doScroll(shadowRoot: ShadowRoot, element: HTMLElement) {
+  shadowRoot.getElementById("val-content-area")?.scrollTo({
+    top: element.offsetTop,
+    behavior: "smooth",
+  });
+}
+
 /**
  * ValRouter was written to emulate the react-router (while also including some useful amenities) which does not work with Next Router
  **/
@@ -57,14 +64,39 @@ export function ValRouter({
         setSourcePath(path as SourcePath);
         // reset scroll position
         const prevScrollPos = historyState.current.pop();
-        setTimeout(() => {
-          const scrollContainer = document
-            .getElementById("val-shadow-root")
-            ?.shadowRoot?.getElementById("val-content-area");
-          if (prevScrollPos) {
-            scrollContainer?.scrollTo(0, prevScrollPos);
-          }
-        }, 50);
+        if (prevScrollPos) {
+          setTimeout(() => {
+            const scrollContainer = document
+              .getElementById("val-shadow-root")
+              ?.shadowRoot?.getElementById("val-content-area");
+            if (prevScrollPos) {
+              scrollContainer?.scrollTo(0, prevScrollPos);
+            }
+          }, 50);
+        } else if (location.hash) {
+          const scrollToId = decodeURIComponent(location.hash.slice(1));
+          // remove hash:
+          window.history.replaceState(
+            null,
+            "",
+            location.pathname + location.search,
+          );
+          let retriesLeft = 100;
+          const execScroll = () => {
+            if (scrollToId) {
+              const shadowRoot =
+                document.getElementById("val-shadow-root")?.shadowRoot;
+              const element = shadowRoot?.getElementById(scrollToId);
+              if (element && shadowRoot) {
+                doScroll(shadowRoot, element);
+              } else if (retriesLeft > 0) {
+                retriesLeft--;
+                setTimeout(execScroll, 100);
+              }
+            }
+          };
+          execScroll();
+        }
       }
       setReady(true);
     };
@@ -86,12 +118,12 @@ export function ValRouter({
           document.getElementById("val-shadow-root")?.shadowRoot;
         const scrollContainer = shadowRoot?.getElementById("val-content-area");
         const prevScrollPos = scrollContainer?.scrollTop;
-        if (params?.scrollToId && shadowRoot) {
+        const scrollId = params?.scrollToId;
+        if (scrollId && shadowRoot) {
           setTimeout(() => {
-            if (params?.scrollToId && shadowRoot) {
-              shadowRoot.getElementById(params.scrollToId)?.scrollIntoView({
-                behavior: "smooth",
-              });
+            const element = shadowRoot.getElementById(scrollId);
+            if (element && shadowRoot) {
+              doScroll(shadowRoot, element);
             }
           }, 100);
         } else {
@@ -107,7 +139,8 @@ export function ValRouter({
           window.history.pushState(null, "", navigateTo);
         }
       } else {
-        window.location.href = navigateTo;
+        window.location.href =
+          navigateTo + (params?.scrollToId ? `#${params.scrollToId}` : "");
       }
     },
     [overlay],
