7758ebd866a88b6641fd67cc42e11664e65e5836
Fredrik Ekholdt <freekh@gmail.com>
2025-05-23 14:36:29 +0200

    Raw now support objects / arrays / etc...

--- packages/next/src/raw.ts
+++ packages/next/src/raw.ts
@@ -1,5 +1,29 @@
+import { RawString } from "@valbuild/core";
 import { ValEncodedString, stegaClean } from "@valbuild/react/stega";
 
-export function raw(encodedString: ValEncodedString): string {
-  return stegaClean(encodedString);
+export type DecodeValEncodedString<T> = T extends ValEncodedString | RawString
+  ? string
+  : T extends { [key: string]: unknown }
+    ? { [K in keyof T]: DecodeValEncodedString<T[K]> }
+    : T extends Array<infer U>
+      ? DecodeValEncodedString<U>[]
+      : T;
+
+export function raw<T>(val: T): DecodeValEncodedString<T> {
+  if (typeof val === "string") {
+    return stegaClean(val) as DecodeValEncodedString<T>;
+  }
+  if (Array.isArray(val)) {
+    return val.map((item) => raw(item)) as DecodeValEncodedString<T>;
+  }
+  if (typeof val === "object" && val !== null) {
+    const result: Record<string, unknown> = {};
+    for (const key in val) {
+      if (Object.prototype.hasOwnProperty.call(val, key)) {
+        result[key] = raw(val[key]);
+      }
+    }
+    return result as DecodeValEncodedString<T>;
+  }
+  return val as DecodeValEncodedString<T>;
 }
