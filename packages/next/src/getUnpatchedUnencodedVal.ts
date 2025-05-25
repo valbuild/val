9e0290b18936f30e5c05f4cda105f4397b8a84b1
Fredrik Ekholdt <freekh@gmail.com>
2025-05-25 09:33:43 +0200

    Add getUnpatchedUnencodedVal for tooling
    Currently unstable since we do not feel confident about the actual name of the method.

---
+++ packages/next/src/getUnpatchedUnencodedVal.ts
@@ -0,0 +1,12 @@
+import { GenericSelector, SelectorOf, SelectorSource } from "@valbuild/core";
+import { StegaOfSource, stegaEncode } from "@valbuild/react/stega";
+
+export function getUnpatchedUnencodedVal<T extends SelectorSource>(
+  selector: T,
+): SelectorOf<T> extends GenericSelector<infer S> ? StegaOfSource<S> : never {
+  // It is weird to use stega encode here since we never actually should stega encode,
+  // however, this is a workaround, since if it is disabled, it will behave as we want
+  return stegaEncode(selector, {
+    disabled: true,
+  });
+}
--- packages/next/src/initVal.ts
+++ packages/next/src/initVal.ts
@@ -9,6 +9,7 @@ import {
   Internal,
 } from "@valbuild/core";
 import { raw } from "./raw";
+import { getUnpatchedUnencodedVal } from "./getUnpatchedUnencodedVal";
 import { decodeValPathOfString } from "./decodeValPathOfString";
 import { ValEncodedString } from "./external_exempt_from_val_quickjs";
 
@@ -18,6 +19,19 @@ export const initVal = (
   config?: ValConfig,
 ): InitVal & {
   val: ValConstructor & {
+    /**
+     * Returns the original module data, without any applied patches or stega encoding.
+     *
+     * This represents the raw, canonical state of the module as it was initially defined.
+     * NOTE: images and files, will be transformed (and will therefore) include the url property.
+     *
+     * ⚠️ Prefer using `fetchVal` (in React Server Components) or `useVal` (in React Client Components)
+     * for most application logic.
+     *
+     * This method is primarily intended for tooling and other advanced use cases
+     * outside of the actual application.
+     */
+    unstable_getUnpatchedUnencodedVal: typeof getUnpatchedUnencodedVal;
     raw: typeof raw;
     attrs: <T extends ValModule<SelectorSource> | Json>(target: T) => ValAttrs;
     unstable_decodeValPathOfString: typeof decodeValPathOfString;
@@ -59,6 +73,7 @@ export const initVal = (
       },
       unstable_decodeValPathOfString: decodeValPathOfString,
       raw,
+      unstable_getUnpatchedUnencodedVal: getUnpatchedUnencodedVal,
     },
     config: currentConfig,
   };
