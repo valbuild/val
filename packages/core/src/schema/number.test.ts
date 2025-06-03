011f0e629987f6de548a2e5a4202047ff7c1ea45
Fredrik Ekholdt <freekh@gmail.com>
2025-06-03 13:29:59 +0200

    Add min / max validations to number

--- packages/core/src/schema/number.test.ts
+++ packages/core/src/schema/number.test.ts
@@ -20,4 +20,52 @@ describe("NumberSchema", () => {
       false,
     );
   });
+
+  test("validate: should return success if src is a number", () => {
+    const schema = number().nullable();
+    const result = schema.validate("foo" as SourcePath, 1 as any);
+    expect(result).toEqual(false);
+  });
+
+  test("validate: should return success if src is within min and max", () => {
+    const schema = number().min(10).max(20);
+    const result = schema.validate("foo" as SourcePath, 15 as any);
+    expect(result).toEqual(false);
+  });
+
+  test("validate: should return errors if src is greater than max", () => {
+    const schema = number().max(10);
+    const result = schema.validate("foo" as SourcePath, 11 as any);
+    expect(result).toMatchObject({
+      foo: [
+        {
+          value: 11,
+        },
+      ],
+    });
+  });
+
+  test("validate: should return errors if src is less than min", () => {
+    const schema = number().min(10);
+    const result = schema.validate("foo" as SourcePath, 9 as any);
+    expect(result).toMatchObject({
+      foo: [
+        {
+          value: 9,
+        },
+      ],
+    });
+  });
+
+  test("validate: should return errors if src is not a number", () => {
+    const schema = number();
+    const result = schema.validate("foo" as SourcePath, "1" as any);
+    expect(result).toMatchObject({
+      foo: [
+        {
+          value: "1",
+        },
+      ],
+    });
+  });
 });
