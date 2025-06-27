0846da7e4c53c2e2680db1417a482be5744c9b15
Fredrik Ekholdt <freekh@gmail.com>
2025-06-27 13:11:32 +0200

    Fix ValImage runtime failures where src is null

--- packages/next/src/ValImage.tsx
+++ packages/next/src/ValImage.tsx
@@ -21,13 +21,13 @@ export type ValImageProps = Omit<
 };
 export function ValImage(props: ValImageProps) {
   const { src, alt, style, width, disableHotspot, height, ...rest } = props;
-  const valPathOfUrl = decodeValPathOfString(src.url);
+  const valPathOfUrl = src?.url && decodeValPathOfString(src.url);
   const valPaths = [valPathOfUrl];
   const maybeValPathOfAlt = decodeValPathOfString(alt as ValEncodedString);
   if (maybeValPathOfAlt) {
     valPaths.push(maybeValPathOfAlt);
   }
-  const hotspot = src.metadata?.hotspot;
+  const hotspot = src?.metadata?.hotspot;
   const imageStyle =
     hotspot && !disableHotspot
       ? {
@@ -36,11 +36,11 @@ export function ValImage(props: ValImageProps) {
         }
       : style;
   const preferMetadataDims =
-    src.metadata !== undefined && !rest.fill && !width && !height;
+    src?.metadata !== undefined && !rest.fill && !width && !height;
   const isUnoptimized =
     rest.unoptimized !== undefined
       ? rest.unoptimized
-      : src.metadata?.mimeType === "image/svg+xml"; // SVGs are unoptimized by default
+      : src?.metadata?.mimeType === "image/svg+xml"; // SVGs are unoptimized by default
   return (
     <NextImage
       {...{
@@ -51,7 +51,7 @@ export function ValImage(props: ValImageProps) {
         lazyBoundary: undefined,
         lazyRoot: undefined,
       }}
-      src={valPathOfUrl ? raw(src.url) : src.url}
+      src={valPathOfUrl ? raw(src?.url) : src?.url}
       data-val-path={valPaths.join(",")}
       data-val-attr-alt={maybeValPathOfAlt}
       data-val-attr-src={valPathOfUrl}
@@ -59,13 +59,13 @@ export function ValImage(props: ValImageProps) {
       alt={
         alt
           ? raw(alt as ValEncodedString)
-          : src.metadata?.alt
-            ? raw(src.metadata?.alt as ValEncodedString)
+          : src?.metadata?.alt
+            ? raw(src?.metadata?.alt as ValEncodedString)
             : ""
       }
       fill={rest.fill}
-      width={preferMetadataDims ? src.metadata?.width : width}
-      height={preferMetadataDims ? src.metadata?.height : height}
+      width={preferMetadataDims ? src?.metadata?.width : width}
+      height={preferMetadataDims ? src?.metadata?.height : height}
       unoptimized={isUnoptimized}
     ></NextImage>
   );
