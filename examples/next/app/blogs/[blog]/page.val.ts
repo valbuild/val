a710e48073ea3bd5bc10cc463e8763b0403fd700
Fredrik Ekholdt <freekh@gmail.com>
2025-07-03 12:35:40 +0200

    Allow circular keyOf for use with .router

--- examples/next/app/blogs/[blog]/page.val.ts
+++ examples/next/app/blogs/[blog]/page.val.ts
@@ -1,15 +1,35 @@
 import { c, nextAppRouter, s } from "_/val.config";
+import { linkSchema } from "../../../components/link";
+import authorsVal from "../../../content/authors.val";
 
 const blogSchema = s.object({
   title: s.string(),
   content: s.string(),
+  author: s.keyOf(authorsVal),
+  link: linkSchema,
 });
 
 export default c.define(
   "/app/blogs/[blog]/page.val.ts",
   s.record(blogSchema).router(nextAppRouter),
   {
-    "/blogs/blog2": { title: "Blog 2", content: "Blog 2 content" },
-    "/blogs/blog1": { title: "Blog 1", content: "Blog 1 content" },
+    "/blogs/blog2": {
+      title: "Blog 2",
+      content: "Blog 2 content",
+      author: "author2",
+      link: {
+        type: "generic",
+        href: "/generic/test/foo",
+      },
+    },
+    "/blogs/blog1": {
+      title: "Blog 1",
+      content: "Blog 1 content",
+      author: "author1",
+      link: {
+        type: "generic",
+        href: "/generic/test/foo",
+      },
+    },
   },
 );
