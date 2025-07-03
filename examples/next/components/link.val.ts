4bcd7f715ae1bca9d2dc597b778be0e8b679e350
Fredrik Ekholdt <freekh@gmail.com>
2025-07-03 21:54:08 +0200

    Simplify example Schema types

--- examples/next/components/link.val.ts
+++ examples/next/components/link.val.ts
@@ -4,27 +4,24 @@ import genericPageVal from "../app/generic/[[...path]]/page.val";
 import { Schema } from "@valbuild/core";
 import pageVal from "../app/page.val";
 
-export const linkSchema: Schema<{
-  type: "main" | "blog" | "generic";
-  href: string;
-}> = s.union(
+export const linkSchema = s.union(
   "type",
   // NOTE: we do `get href() { ... }` to avoid circular dependencies
   s.object({
     type: s.literal("main"),
-    get href() {
+    get href(): Schema<string> {
       return s.keyOf(pageVal);
     },
   }),
   s.object({
     type: s.literal("blog"),
-    get href() {
+    get href(): Schema<string> {
       return s.keyOf(blogsVal);
     },
   }),
   s.object({
     type: s.literal("generic"),
-    get href() {
+    get href(): Schema<string> {
       return s.keyOf(genericPageVal);
     },
   }),
