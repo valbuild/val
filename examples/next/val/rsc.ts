519b8b9fa06aa3ee9ba6a6abec6a9c628e7255f5
Fredrik Ekholdt <freekh@gmail.com>
2025-08-31 19:01:04 +0200

    Add useValRouteUrl and fetchValUrl

--- examples/next/val/rsc.ts
+++ examples/next/val/rsc.ts
@@ -4,11 +4,14 @@ import { config } from "../val.config";
 import valModules from "../val.modules";
 import { cookies, draftMode, headers } from "next/headers";
 
-const { fetchValStega: fetchVal, fetchValRouteStega: fetchValRoute } =
-  initValRsc(config, valModules, {
-    draftMode,
-    headers,
-    cookies,
-  });
+const {
+  fetchValStega: fetchVal,
+  fetchValRouteStega: fetchValRoute,
+  fetchValRouteUrl,
+} = initValRsc(config, valModules, {
+  draftMode,
+  headers,
+  cookies,
+});
 
-export { fetchVal, fetchValRoute };
+export { fetchVal, fetchValRoute, fetchValRouteUrl };
