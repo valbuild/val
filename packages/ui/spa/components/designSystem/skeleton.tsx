c88610bb772d7a1484b48a0f337d0e702a1758d3
Fredrik Ekholdt <freekh@gmail.com>
2026-01-12 14:10:58 +0100

    Format

--- packages/ui/spa/components/designSystem/skeleton.tsx
+++ packages/ui/spa/components/designSystem/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "./cn"
+import { cn } from "./cn";
 
 function Skeleton({
   className,
@@ -9,7 +9,7 @@ function Skeleton({
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
