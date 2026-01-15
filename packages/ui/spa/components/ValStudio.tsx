57b16b8a61b350acceffc599fef04f06c497e7ef
Fredrik Ekholdt <freekh@gmail.com>
2026-01-15 10:58:56 +0100

    Fix broken overlay

--- packages/ui/spa/components/ValStudio.tsx
+++ packages/ui/spa/components/ValStudio.tsx
@@ -1,8 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ValClient } from "@valbuild/shared/src/internal/ValClient";
-import { ValProvider } from "./ValProvider";
+import { Themes, ValProvider } from "./ValProvider";
 import { Layout } from "./Layout";
-import { SharedValConfig } from "@valbuild/shared/internal";
+import {
+  SharedValConfig,
+  VAL_THEME_SESSION_STORAGE_KEY,
+} from "@valbuild/shared/internal";
 import { ValRouter } from "./ValRouter";
 
 interface ValFullscreenProps {
@@ -11,11 +14,31 @@ interface ValFullscreenProps {
 }
 
 export const ValStudio: FC<ValFullscreenProps> = ({ client, config }) => {
+  // Theme is initialized by ValNextProvider in session storage
+  // We just read it once on init and then rely on React state
+  const [theme, setTheme] = useState<Themes | null>(() => {
+    const storedTheme = sessionStorage.getItem(VAL_THEME_SESSION_STORAGE_KEY);
+    if (storedTheme === "light" || storedTheme === "dark") {
+      return storedTheme;
+    }
+    return null;
+  });
   return (
-    <ValProvider client={client} dispatchValEvents={false} config={config}>
-      <ValRouter>
-        <Layout />
-      </ValRouter>
+    <ValProvider client={client} dispatchValEvents={false} config={config} theme={theme} setTheme={setTheme}>
+      <div
+        {...(theme ? { "data-mode": theme } : {})}
+        className="bg-bg-primary font-sans text-fg-primary"
+        style={{
+          minHeight: "100svh",
+          width: "100vw",
+          visibility: "hidden",
+        }}
+        id="val-app-container"
+      >
+        <ValRouter>
+          <Layout />
+        </ValRouter>
+      </div>
     </ValProvider>
   );
 };
