fccc14d363cd91d81c1a14f5186cb82f7497bb87
Fredrik Ekholdt <freekh@gmail.com>
2026-01-17 17:49:50 +0100

    Nicer list preview items: smaller and less bold titles

---
+++ packages/ui/spa/components/ListPreviewItem.tsx
@@ -0,0 +1,79 @@
+import {
+  ImageSource,
+  ImageMetadata,
+  Internal,
+  RemoteSource,
+  VAL_EXTENSION,
+} from "@valbuild/core";
+import { cn } from "./designSystem/cn";
+import { useState } from "react";
+
+export function ListPreviewItem({
+  title,
+  image,
+  subtitle,
+}: {
+  title: string;
+  image: ImageSource | RemoteSource<ImageMetadata> | null;
+  subtitle: string | null;
+}) {
+  return (
+    <div
+      className={cn(
+        "flex w-full items-start justify-between pl-4 flex-grow text-left",
+      )}
+    >
+      <div className="flex flex-col flex-shrink py-4 overflow-x-clip">
+        <div className="font-semibold">{title}</div>
+        {subtitle && (
+          <div className="block overflow-hidden flex-shrink max-h-5 text-sm text-gray-500 text-ellipsis">
+            {subtitle}
+          </div>
+        )}
+      </div>
+      {image && <ImageOrPlaceholder src={image} alt={title} />}
+    </div>
+  );
+}
+
+function ImageOrPlaceholder({
+  src,
+  alt,
+}: {
+  src: ImageSource | RemoteSource<ImageMetadata> | null | undefined;
+  alt: string;
+}) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  if (src === null || src === undefined) {
+    return (
+      <div className="flex-shrink-0 ml-4 w-20 h-20 opacity-25 bg-bg-brand-secondary"></div>
+    );
+  }
+
+  const imageUrl =
+    src[VAL_EXTENSION] === "file"
+      ? Internal.convertFileSource(src).url
+      : Internal.convertRemoteSource(src).url;
+
+  return (
+    <div className="relative flex-shrink-0 ml-4 w-20 h-20">
+      {!isLoaded && (
+        <div className="absolute inset-0 opacity-25 bg-bg-brand-secondary animate-in"></div>
+      )}
+      <img
+        src={imageUrl}
+        alt={alt}
+        onLoad={() => setIsLoaded(true)}
+        onError={() => setIsLoaded(false)}
+        className={`absolute inset-0 object-cover w-full h-full rounded-r-lg ${isLoaded ? "opacity-100" : "opacity-0"}`}
+        style={{
+          objectPosition: src.metadata?.hotspot
+            ? `${src.metadata.hotspot.x}% ${src.metadata.hotspot.y}%`
+            : "",
+          transition: "opacity 0.2s ease-in-out",
+        }}
+      />
+    </div>
+  );
+}
--- packages/ui/spa/components/SortableList.tsx
+++ packages/ui/spa/components/SortableList.tsx
@@ -40,6 +40,7 @@ import {
 } from "./designSystem/popover";
 import { cn } from "./designSystem/cn";
 import { FieldValidationError } from "./FieldValidationError";
+import { ListPreviewItem } from "./ListPreviewItem";
 
 export function SortableList({
   source,
@@ -249,7 +250,13 @@ export function SortableItem({
             onClick(path);
           }}
         >
-          {render && <ListPreviewItem {...render} />}
+          {render && (
+            <ListPreviewItem
+              title={render.title}
+              image={render.image ?? null}
+              subtitle={render.subtitle ?? null}
+            />
+          )}
           {!render && (
             <div className="flex-grow p-4 w-full">
               <AutoPreview path={path} />
@@ -310,70 +317,6 @@ export function SortableItem({
           </button>
         </PopoverContent>
       </Popover>
-    </div>
-  );
-}
-
-function ListPreviewItem({
-  title,
-  image,
-  subtitle,
-}: ListArrayRender["items"][number]) {
-  return (
-    <div
-      className={cn("flex w-full items-center justify-between pl-4 flex-grow")}
-    >
-      <div className="flex flex-col flex-shrink py-4 overflow-x-clip">
-        <div className="font-medium">{title}</div>
-        {subtitle && (
-          <div className="block overflow-hidden flex-shrink max-h-5 text-sm text-gray-500 text-ellipsis">
-            {subtitle}
-          </div>
-        )}
-      </div>
-      {image && <ImageOrPlaceholder src={image} alt={title} />}
-    </div>
-  );
-}
-
-function ImageOrPlaceholder({
-  src,
-  alt,
-}: {
-  src: ImageSource | RemoteSource<ImageMetadata> | null | undefined;
-  alt: string;
-}) {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  if (src === null || src === undefined) {
-    return (
-      <div className="flex-shrink-0 ml-4 w-20 h-20 opacity-25 bg-bg-brand-secondary"></div>
-    );
-  }
-
-  const imageUrl =
-    src[VAL_EXTENSION] === "file"
-      ? Internal.convertFileSource(src).url
-      : Internal.convertRemoteSource(src).url;
-
-  return (
-    <div className="relative flex-shrink-0 ml-4 w-20 h-20">
-      {!isLoaded && (
-        <div className="absolute inset-0 opacity-25 bg-bg-brand-secondary animate-in"></div>
-      )}
-      <img
-        src={imageUrl}
-        alt={alt}
-        onLoad={() => setIsLoaded(true)}
-        onError={() => setIsLoaded(false)}
-        className={`absolute inset-0 object-cover w-full h-full rounded-r-lg ${isLoaded ? "opacity-100" : "opacity-0"}`}
-        style={{
-          objectPosition: src.metadata?.hotspot
-            ? `${src.metadata.hotspot.x}% ${src.metadata.hotspot.y}%`
-            : "",
-          transition: "opacity 0.2s ease-in-out",
-        }}
-      />
     </div>
   );
 }
