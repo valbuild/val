523c738516d3638e42a489224d14492cc3192968
Fredrik Ekholdt <freekh@gmail.com>
2026-02-07 12:19:06 +0100

    Enrich c.image and c.file with metadata from s.images and s.files

--- packages/core/src/module.ts
+++ packages/core/src/module.ts
@@ -9,7 +9,7 @@ import {
   Path,
 } from "./selector";
 import { Source } from "./source";
-import { ModuleFilePath, ModulePath, SourcePath } from "./val";
+import { getValPath, ModuleFilePath, ModulePath, SourcePath } from "./val";
 import { ArraySchema, SerializedArraySchema } from "./schema/array";
 import { UnionSchema, SerializedUnionSchema } from "./schema/union";
 import { Json } from "./Json";
@@ -19,13 +19,13 @@ import {
   ImageSchema,
   SerializedImageSchema,
 } from "./schema/image";
-import { FileSource } from "./source/file";
+import { FILE_REF_PROP, FileSource } from "./source/file";
 import { AllRichTextOptions, RichTextSource } from "./source/richtext";
 import { RecordSchema, SerializedRecordSchema } from "./schema/record";
 import { RawString } from "./schema/string";
 import { ImageSelector } from "./selector/image";
 import { ImageSource } from "./source/image";
-import { ModuleFilePathSep } from ".";
+import { FileMetadata, FileSchema, ModuleFilePathSep } from ".";
 
 const brand = Symbol("ValModule");
 export type ValModule<T extends SelectorSource> = SelectorOf<T> &
@@ -55,9 +55,7 @@ export type ReplaceRawStringWithString<T extends SelectorSource> =
 
 export function define<T extends Schema<SelectorSource>>(
   id: string, // TODO: `/${string}`
-
   schema: T,
-
   source: ReplaceRawStringWithString<SelectorOfSchema<T>>,
 ): ValModule<SelectorOfSchema<T>> {
   return {
@@ -65,6 +63,82 @@ export function define<T extends Schema<SelectorSource>>(
     [GetSchema]: schema,
     [Path]: id as SourcePath,
   } as unknown as ValModule<SelectorOfSchema<T>>;
+}
+
+export function enrichFileImageSourceWithRemote(
+  source: SelectorSource,
+  schema: Schema<SelectorSource>,
+): SelectorSource {
+  const addedModules = new Set<string>();
+  let filesLookup: Record<string, ImageMetadata | FileMetadata> = {};
+  function traverseSchema(schema: Schema<SelectorSource>) {
+    if (schema instanceof ImageSchema || schema instanceof FileSchema) {
+      const moduleMetadata =
+        schema instanceof ImageSchema
+          ? schema["moduleMetadata"]
+          : schema["moduleMetadata"];
+      for (const [modulePath, valModule] of Object.entries(
+        moduleMetadata || {},
+      )) {
+        if (addedModules.has(modulePath)) {
+          continue;
+        }
+        addedModules.add(modulePath);
+        filesLookup = {
+          ...filesLookup,
+          ...valModule,
+        };
+      }
+    } else if (schema instanceof ObjectSchema) {
+      for (const value of Object.values(schema["items"])) {
+        if (value instanceof Schema) {
+          traverseSchema(value);
+        }
+      }
+    } else if (schema instanceof ArraySchema) {
+      traverseSchema(schema["item"]);
+    } else if (schema instanceof RecordSchema) {
+      traverseSchema(schema["item"]);
+    } else if (schema instanceof UnionSchema) {
+      for (const value of schema["items"]) {
+        if (value instanceof ObjectSchema) {
+          traverseSchema(value);
+        }
+      }
+    } else if (schema instanceof RichTextSchema) {
+      if (schema["options"]?.inline?.img instanceof ImageSchema) {
+        traverseSchema(schema["options"].inline.img);
+      }
+    }
+  }
+  function injectMetadataIntoSource(source: SelectorSource): SelectorSource {
+    if (!source) {
+      return source;
+    }
+    if (
+      typeof source === "object" &&
+      FILE_REF_PROP in source &&
+      typeof source[FILE_REF_PROP] === "string"
+    ) {
+      const fileRef = source[FILE_REF_PROP];
+      const metadata = filesLookup[fileRef];
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (source as any).metadata = metadata;
+    } else if (typeof source === "object") {
+      if (Array.isArray(source)) {
+        for (const item of source) {
+          injectMetadataIntoSource(item);
+        }
+      } else {
+        for (const value of Object.values(source)) {
+          injectMetadataIntoSource(value);
+        }
+      }
+    }
+  }
+  traverseSchema(schema);
+  injectMetadataIntoSource(source);
+  return source;
 }
 
 export function getSource(valModule: ValModule<SelectorSource>): Source {
