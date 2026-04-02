8d2da030503d1e5e7f13c5c097da55d788d9764a
Fredrik Ekholdt <freekh@gmail.com>
2026-04-02 15:44:13 +0200

    Increase size of grid view items

--- packages/ui/spa/components/FileGallery/FileGallery.tsx
+++ packages/ui/spa/components/FileGallery/FileGallery.tsx
@@ -265,7 +265,7 @@ export function FileGallery({
             ))}
           </div>
         ) : viewMode === "grid" ? (
-          <div className="grid grid-cols-4 gap-2 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12">
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {filteredFiles.map((file, index) => (
               <FileGalleryItem
                 key={`${file.folder}/${file.filename}`}
