e8f1b9683fd97dec89a7ca304776649a954e3301
Fredrik Ekholdt <freekh@gmail.com>
2026-04-15 22:01:16 +0200

    Rm console.log

--- packages/server/src/ValServer.ts
+++ packages/server/src/ValServer.ts
@@ -1590,13 +1590,11 @@ export const ValServer = (
           patchIds,
           excludePatchOps: false,
         });
-        console.log("Got patches for save", patches);
         const analysis = serverOps.analyzePatches(
           patches.patches,
           patches.commits,
           commit,
         );
-        console.log("Got analysis for save", analysis);
         const preparedCommit = await serverOps.prepare({
           ...analysis,
           ...patches,
