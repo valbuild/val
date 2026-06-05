e00564faf8488d8c76cae821c2cdf0d8eac3a0a1
Fredrik Ekholdt <freekh@gmail.com>
2026-06-05 17:32:44 +0200

    Fix richtext where all options seems to be shown as available in toolbar

--- packages/ui/spa/components/RichTextEditor/convertOptions.ts
+++ packages/ui/spa/components/RichTextEditor/convertOptions.ts
@@ -7,25 +7,21 @@ export function serializedRichTextOptionsToFeatures(
   if (!options) return {};
   const features: Partial<EditorFeatures> = {};
 
-  if (options.style) {
-    features.bold = options.style.bold ?? false;
-    features.italic = options.style.italic ?? false;
-    features.strikethrough = options.style.lineThrough ?? false;
-  }
-  if (options.block) {
-    features.h1 = options.block.h1 ?? false;
-    features.h2 = options.block.h2 ?? false;
-    features.h3 = options.block.h3 ?? false;
-    features.h4 = options.block.h4 ?? false;
-    features.h5 = options.block.h5 ?? false;
-    features.h6 = options.block.h6 ?? false;
-    features.bulletList = options.block.ul ?? false;
-    features.orderedList = options.block.ol ?? false;
-  }
-  if (options.inline) {
-    features.link = !!options.inline.a;
-    features.image = !!options.inline.img;
-  }
+  features.bold = options.style?.bold ?? false;
+  features.italic = options.style?.italic ?? false;
+  features.strikethrough = options.style?.lineThrough ?? false;
+
+  features.h1 = options.block?.h1 ?? false;
+  features.h2 = options.block?.h2 ?? false;
+  features.h3 = options.block?.h3 ?? false;
+  features.h4 = options.block?.h4 ?? false;
+  features.h5 = options.block?.h5 ?? false;
+  features.h6 = options.block?.h6 ?? false;
+  features.bulletList = options.block?.ul ?? false;
+  features.orderedList = options.block?.ol ?? false;
+
+  features.link = !!options.inline?.a;
+  features.image = !!options.inline?.img;
 
   features.hardBreak = true;
   features.fixedToolbar = true;
